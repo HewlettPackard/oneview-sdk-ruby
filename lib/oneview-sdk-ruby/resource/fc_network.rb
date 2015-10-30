a4dfe8b44c768a3ac5be93e06585d5e2d09a62ef
vagrant <henrique.diomede@gmail.com>
2015-10-30 11:54:30 +0000

    #5 Porting example and resource to new syntax

--- lib/oneview-sdk-ruby/resource/fc_network.rb
+++ lib/oneview-sdk-ruby/resource/fc_network.rb
@@ -1,49 +1,50 @@
 
 module OneviewSDK
   # Resource for ethernet networks
+  # Common Data Attributes
+  #   autoLoginRedistribution (Required)
+  #   category
+  #   connectionTemplateUri (Required)
+  #   created
+  #   description
+  #   eTag
+  #   fabricType (Required)
+  #   linkStabilityTime (Required)
+  #   managedSunUri
+  #   modified
+  #   name
+  #   state
+  #   status
+  #   type (Required)
+  #   uri
+  
   class FCNetwork < Resource
     BASE_URI = '/rest/fc-networks'
 
-    attr_accessor \
-      :autoLoginRedistribution,
-      :category,
-      :connectionTemplateUri,
-      :created,
-      :description,
-      :eTag,
-      :fabricType,
-      :linkStabilityTime,
-      :managedSunUri,
-      :modified,
-      :name,
-      :state,
-      :status,
-      :type,
-      :uri
-
     def initialize(client, params = {}, api_ver = nil)
-      # Default values
-      @autoLoginRedistribution = false
-      @type = 'fc-networkV2'
-      @linkStabilityTime = 30
-      @fabricType = 'FabricAttach'
       super
+
+      # Default values
+      @data['autoLoginRedistribution'] ||= false
+      @data['type'] ||= 'fc-networkV2'
+      @data['linkStabilityTime'] ||= 30
+      @data['fabricType'] ||= 'FabricAttach'
     end
 
-    def fabricType=(value)
-      if %w(DirectAttach FabricAttach).include?(value)
-        @fabricType = value
-      else
-        fail 'Invalid fabricType value'
-      end
+
+    def validate(params = {})
+      # fail "Invalid Fabric type #{params['fabricType']}" unless fabricType(params['fabricType'])
+      # fail 'Link stability out of range 1..1800' unless linkStabilityTime(params['linkStabilityTime'])
     end
 
-    def linkStabilityTime=(value)
-      if value.between?(1, 1800)
-        @linkStabilityTime = value
-      else
-        fail 'linkStabilityTime out of range 1..1800'
-      end
+    private
+
+    def fabricType(value)
+      %w(DirectAttach FabricAttach).include?(value)
+    end
+
+    def linkStabilityTime(value)
+      value.between?(1, 1800)
     end
 
   end
