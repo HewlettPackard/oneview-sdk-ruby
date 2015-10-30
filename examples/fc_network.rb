495832ef1982d08fd7ccc9064cc17d14f082d9ec
vagrant <henrique.diomede@gmail.com>
2015-10-30 17:23:39 +0000

    #5 Changed resource validation rules

--- examples/fc_network.rb
+++ examples/fc_network.rb
@@ -13,26 +13,26 @@ options = {
 
 fc = OneviewSDK::FCNetwork.new(@client, options)
 fc.create
-puts "\nCreated fc-network '#{fc.name}' sucessfully.\n  uri = '#{fc.uri}'"
+puts "\nCreated fc-network '#{fc[:name]}' sucessfully.\n  uri = '#{fc[:uri]}'"
 
 # Find recently created network by name
-matches = OneviewSDK::FCNetwork.find_by(@client, name: fc.name)
+matches = OneviewSDK::FCNetwork.find_by(@client, name: fc[:name])
 fc2 = matches.first
-puts "\nFound ethernet-network by name: '#{fc2.name}'.\n  uri = '#{fc2.uri}'"
+puts "\nFound ethernet-network by name: '#{fc2[:name]}'.\n  uri = '#{fc2[:uri]}'"
 
 # Retrieve recently created network
-fc3 = OneviewSDK::FCNetwork.new(@client, name: fc.name)
+fc3 = OneviewSDK::FCNetwork.new(@client, name: fc[:name])
 fc3.retrieve!
-puts "\nRetrieved ethernet-network data by name: '#{fc3.name}'.\n  uri = '#{fc3.uri}'"
+puts "\nRetrieved ethernet-network data by name: '#{fc3[:name]}'.\n  uri = '#{fc3[:uri]}'"
 
 # Delete this network
 fc3.delete
-puts "\nSucessfully deleted fc-network '#{fc3.name}'."
+puts "\nSucessfully deleted fc-network '#{fc3[:name]}'."
 
 
 # Example: List all fc networks with certain attributes
 attributes = { fabricType: 'FabricAttach' }
 puts "\n\nFC networks with #{attributes}"
 OneviewSDK::FCNetwork.find_by(@client, attributes).each do |network|
-  puts "  #{network.name}"
+  puts "  #{network[:name]}"
 end
--- lib/oneview-sdk-ruby/resource/fc_network.rb
+++ lib/oneview-sdk-ruby/resource/fc_network.rb
@@ -1,29 +1,27 @@
 
 module OneviewSDK
   # Resource for ethernet networks
-  # Common Data Attributes
-  #   autoLoginRedistribution (Required)
+  # Common Data Attributes:
+  #   autoLoginRedistribution
   #   category
-  #   connectionTemplateUri (Required)
+  #   connectionTemplateUri
   #   created
   #   description
   #   eTag
-  #   fabricType (Required)
-  #   linkStabilityTime (Required)
+  #   fabricType
+  #   linkStabilityTime
   #   managedSunUri
   #   modified
   #   name
   #   state
   #   status
-  #   type (Required)
+  #   type
   #   uri
-  
   class FCNetwork < Resource
     BASE_URI = '/rest/fc-networks'
 
     def initialize(client, params = {}, api_ver = nil)
       super
-
       # Default values
       @data['autoLoginRedistribution'] ||= false
       @data['type'] ||= 'fc-networkV2'
@@ -31,20 +29,16 @@ module OneviewSDK
       @data['fabricType'] ||= 'FabricAttach'
     end
 
-
-    def validate(params = {})
-      # fail "Invalid Fabric type #{params['fabricType']}" unless fabricType(params['fabricType'])
-      # fail 'Link stability out of range 1..1800' unless linkStabilityTime(params['linkStabilityTime'])
+    def validate_fabricType(value)
+      fail 'Invalid fabric type' unless %w(DirectAttach FabricAttach).include?(value)
     end
 
-    private
-
-    def fabricType(value)
-      %w(DirectAttach FabricAttach).include?(value)
+    def validate_linkStabilityTime(value)
+      fail 'Link stability time out of range 1..1800' unless value.between?(1, 1800)
     end
-
-    def linkStabilityTime(value)
-      value.between?(1, 1800)
+    
+    def validate_status(value)
+      fail 'Invalid status' unless %w(OK Disabled Warning Critical Unknown).include?(value)
     end
 
   end
