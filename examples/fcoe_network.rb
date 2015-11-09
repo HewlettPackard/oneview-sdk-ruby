c657c19c8e4c9f190813585127c1872e2b534b82
tmiotto <thiagomiottoam@gmail.com>
2015-11-09 12:31:14 -0200

    Simplified example fcoe_network

--- examples/fcoe_network.rb
+++ examples/fcoe_network.rb
@@ -9,23 +9,10 @@ options = {
   vlanId: 300
 }
 
-disabled_options = {
-  name: 'OneViewSDK Disabled FCoE Network',
-  connectionTemplateUri: nil,
-  type: 'fcoe-network',
-  vlanId: 301,
-  status: 'Disabled'
-}
-
 # Sucess case - 1
 fcoe = OneviewSDK::FCoENetwork.new(@client, options)
 fcoe.create
 puts "\nCreated fcoe-network '#{fcoe[:name]}' sucessfully.\n  uri = '#{fcoe[:uri]}'"
-
-# Success case - 2
-dis_fcoe = OneviewSDK::FCoENetwork.new(@client, disabled_options)
-dis_fcoe.create
-puts "\nCreated fcoe-network '#{dis_fcoe[:name]}' sucessfully.\n  uri = '#{dis_fcoe[:uri]}'"
 
 # Find recently created network by name
 matches = OneviewSDK::FCoENetwork.find_by(@client, name: fcoe[:name])
@@ -38,7 +25,7 @@ fcoe3.retrieve!
 puts "\nRetrieved ethernet-network data by name: '#{fcoe3[:name]}'.\n  uri = '#{fcoe3[:uri]}'"
 
 # Example: List all fcoe networks with certain attributes
-attributes = { status: 'Disabled' }
+attributes = { status: 'OK' }
 puts "\n\nFCoE networks with #{attributes}"
 OneviewSDK::FCoENetwork.find_by(@client, attributes).each do |network|
   puts "  #{network[:name]}"
