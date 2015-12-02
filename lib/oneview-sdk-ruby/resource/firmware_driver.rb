186a949110c96e0bae4b5489f7fc6d1393677569
Jared Smartt <jared.smartt@hp.com>
2015-12-02 14:09:12 -0700

    Added firmware bundle & driver resources

---
+++ lib/oneview-sdk-ruby/resource/firmware_driver.rb
@@ -0,0 +1,49 @@
+
+module OneviewSDK
+  # Resource for server profiles
+  # Common Data Attributes:
+  #   baselineShortName
+  #   bundleSize
+  #   category
+  #   description
+  #   eTag
+  #   fwComponents
+  #   isoFileName
+  #   lastTaskUri
+  #   name
+  #   releaseDate
+  #   resourceId
+  #   resourceState
+  #   state
+  #   status
+  #   supportedLanguages
+  #   supportedOSList
+  #   swPackagesFullPath
+  #   type (Required)
+  #   uri
+  #   uuid
+  #   version
+  #   xmlKeyName
+  class FirmwareDriver < Resource
+    BASE_URI = '/rest/firmware-drivers'
+
+    def initialize(client, params = {}, api_ver = nil)
+      super
+      # Default values
+      @data['type'] ||= 'firmware-baselines'
+    end
+
+    def create
+      fail 'Method not available for this resource!'
+    end
+
+    def update
+      create
+    end
+
+    def save
+      create
+    end
+
+  end
+end
---
+++ spec/unit/resource/firmware_driver_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+RSpec.describe OneviewSDK::FirmwareDriver do
+  include_context 'shared context'
+
+  describe '#initialize' do
+    it 'sets the type correctly' do
+      profile = OneviewSDK::FirmwareDriver.new(@client)
+      expect(profile[:type]).to eq('firmware-baselines')
+    end
+  end
+
+  describe 'undefined methods' do
+    before :each do
+      @item = OneviewSDK::FirmwareDriver.new(@client, {})
+    end
+
+    it 'does not allow the create action' do
+      expect { @item.create }.to raise_error(/not available for this resource/)
+    end
+
+    it 'does not allow the update action' do
+      expect { @item.update }.to raise_error(/not available for this resource/)
+    end
+
+    it 'does not allow the save action' do
+      expect { @item.save }.to raise_error(/not available for this resource/)
+    end
+  end
+end
