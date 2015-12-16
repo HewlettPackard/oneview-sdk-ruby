476663fbf0fa86117e86ab2cae30729300fe0cbb
tmiotto <thiagomiottoam@gmail.com>
2015-12-16 14:35:20 -0200

    Volumes base implementation

---
+++ examples/volume.rb
@@ -0,0 +1,89 @@
+require_relative '_client'
+
+# Example: Create Volumes from the creation mehtods
+# NOTE: This will use a few volume systems and create 6 volumes, one for each creation type
+
+# 1) Common = Storage System + Storage Pool
+puts '1) Common = Storage System + Storage Pool'
+
+options1 = {
+  name: 'ONEVIEW_SDK_TEST_VOLUME_1',
+  description: 'Test volume with common creation: Storage System + Storage Pool'
+}
+
+volume1 = OneviewSDK::Volume.new(@client, options1)
+
+storage_system = OneviewSDK::StorageSystem.new(@client, credentials: { ip_hostname: '172.18.11.11' })
+storage_system.retrieve!
+
+volume1.add_storage_system(storage_system)
+
+# Create storage pool
+options_pool = {
+  storageSystemUri: storage_system[:uri],
+  name: 'FST_CPG1'
+}
+
+storage_pool = OneviewSDK::StoragePool.new(@client, options_pool)
+storage_pool.set_storage_system(storage_system)
+storage_pool.retrieve!
+
+volume1.add_storage_pool(storage_pool)
+
+volume1.set_provision_type  # Full
+volume1.set_shareable  # shareable
+volume1.set_requested_capacity(512*1024*1024)  # 512MB
+
+volume1.create!
+puts "Created #{volume1['name']}"
+
+
+# 2) Template = Storage Volume Template
+# CANNOT IMPLEMENT: Missing Storage Volume Templates resource
+puts '2) Template = Storage Volume Template'
+puts 'Method not implemented'
+
+
+# 3) Common with snapshots = Storage System + Storage Pool + Snapshot Pool
+puts '3) Common with snapshots = Storage System + Storage Pool + Snapshot Pool'
+
+options3 = {
+  name: 'ONEVIEW_SDK_TEST_VOLUME_3',
+  description: 'Test volume - common creation with snapshot pool: Storage System + Storage Pool + Snapshot Pool'
+}
+
+volume3 = OneviewSDK::Volume.new(@client, options3)
+
+volume3.add_storage_system(storage_system)
+
+# Provisioning Parameters
+volume3.add_storage_pool(storage_pool)
+volume3.set_provision_type('Thin')
+volume3.set_shareable(false)  # private
+volume3.set_requested_capacity(1024*1024*1024)  # 1GB
+
+# The same snapshot pool of the storage pool
+volume3.add_snapshot_pool(storage_pool)
+
+volume3.create!
+puts "Created #{volume3['name']}"
+
+
+# 4) Management = Storage System + wwn
+puts '4) Management = Storage System + wwn'
+
+options4 = {
+  name: 'ONEVIEW_SDK_TEST_VOLUME_4',
+  description: 'Test volume - management creation: Storage System + wwn',
+  wwn: 'DC:83:80:27:56:00:10:00:30:71:45:02:08:86:41:52' # Need unmanaged volume
+}
+
+volume4 = OneviewSDK::Volume.new(@client, options4)
+
+volume4.add_storage_system(storage_system)
+
+# Provisioning Parameters
+volume4.set_shareable(false)  # private
+
+volume4.create!
+puts "Created #{volume4['name']}"
--- lib/oneview-sdk-ruby/resource/volume.rb
+++ lib/oneview-sdk-ruby/resource/volume.rb
@@ -17,12 +17,6 @@ module OneviewSDK
   #   wwn
   class Volume < Resource
     BASE_URI = '/rest/storage-volumes'
-    PROVISIONING_PARAMETERS_LIST = %w(provisionType requestedCapacity shareable storagePoolUri)
-
-    def initialize(client, params = {}, api_ver = nil)
-      super
-      # Default values
-    end
 
     # It's possible to create the volume in 6 different ways:
     # 1) Common = Storage System + Storage Pool
@@ -31,39 +25,65 @@ module OneviewSDK
     # 4) Management = Storage System + wwn
     # 5) Management by name = Storage System + Storage System Volume Name
     # 6) Snapshot = Snapshot Pool + Storage Pool + Snapshot
-    def create
+    def initialize(client, params = {}, api_ver = nil)
       super
+      # Default values
+      @data['provisioningParameters'] ||= {}
     end
 
-    # Set a resource attribute with the given value and call any validation method if necessary
-    # @param [String] key attribute name
-    # @param value value to assign to the given attribute
-    # @note Keys will be converted to strings
-    # @override Some params are redirected to nested hash inside options
-    def set(key, value)
-      method_name = "validate_#{key}"
-      send(method_name.to_sym, value) if self.respond_to?(method_name.to_sym)
-      if PROVISIONING_PARAMETERS_LIST.include?(key.to_s)
-        @data['provisioningParameters'] = {} unless @data['provisioningParameters']
-        @data['provisioningParameters'][key.to_s] = value
-      else
-        @data[key.to_s] = value
-      end
-    end
-
+    # Adds storage system to the volume creation
+    # @param [OneviewSDK::Resource] Storage system to create the volume
     def add_storage_system(storage_system)
+      assure_uri(storage_system)
+      @data['storageSystemUri'] = storage_system['uri']
     end
 
+    # Adds storage pool to the volume creation inside provisioningParameters
+    # @param [OneviewSDK::Resource] Storage pool to create the volume
     def add_storage_pool(storage_pool)
+      assure_uri(storage_pool)
+      @data['provisioningParameters']['storagePoolUri'] = storage_pool['uri']
     end
 
+    # Adds storage volume template  to the volume creation
+    # @param [OneviewSDK::Resource] Storage volume template to create the volume
     def set_storage_volume_template(storage_volume_template)
+      assure_uri(storage_volume_template)
+      @data['templateUri'] = storage_volume_template['uri']
     end
 
+    # Adds snapshot pool to the volume creation
+    # @param [OneviewSDK::Resource] Snapshot pool to create the volume
     def add_snapshot_pool(storage_pool)
+      assure_uri(storage_pool)
+      @data['snapshotPoolUri'] = storage_pool['uri']
     end
 
     def add_snapshot(storage_volume_snapshot)
+      # TODO Need sub-resource snapshot
+    end
+
+    # Defines the volume capacity
+    # @param [Fixnum] The required capacity in Bytes.
+    def set_requested_capacity(capacity)
+      @data['provisioningParameters']['requestedCapacity'] = capacity
+    end
+
+    # Defines the type of provisioning
+    # @param [String] The desired provisioning type. Must be Thin or Full. Default is Full.
+    def set_provision_type(type='Full')
+      validate_provisionType(type)
+      @data['provisioningParameters']['provisionType'] = type
+    end
+
+    # Defines the type of provisioning
+    # @param [FalseClass|TrueClass] The shareability of the volume. Default is true.
+    def set_shareable(share=true)
+      @data['provisioningParameters']['shareable'] = share
+    end
+
+    def validate_provisionType(value)
+      fail 'Invalid provision type' unless %w(Thin Full).include?(value)
     end
 
     def validate_fabricType(value)
@@ -73,6 +93,15 @@ module OneviewSDK
     def validate_linkStabilityTime(value)
       return unless @data['fabricType'] && @data['fabricType'] == 'FabricAttach'
       fail 'Link stability time out of range 1..1800' unless value.between?(1, 1800)
+    end
+
+    private
+
+    # Verify if the resource has a URI
+    # If not, first it tries to retrieve, and then verify for its existence
+    def assure_uri(resource)
+      resource.retrieve! unless resource['uri']
+      fail "#{resource.class}: #{resource['name']} not found" unless resource['uri']
     end
 
   end
