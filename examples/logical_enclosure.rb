a6170ad52f7f183d5690c01b77f6d05f2e371926
Jared Smartt <jared.smartt@hpe.com>
2015-12-17 17:05:50 +0000

    updated method names & added tests

--- examples/logical_enclosure.rb
+++ examples/logical_enclosure.rb
@@ -1,32 +1,35 @@
-require_relative '_client'
+require_relative '_client' # Gives access to @client
 
 
 # Get first logical enclosure
 logical_enclosure = OneviewSDK::LogicalEnclosure.find_by(@client, {}).first
-puts "Found logical-enclosure '#{logical_enclosure[:name]}'.\n uri = '#{logical_enclosure[:uri]}'"
+puts "Found logical-enclosure '#{logical_enclosure[:name]}'."
 
-# Retrieve logical enclosure with name 'Encl2'
-logical_enclosure_2 = OneviewSDK::LogicalEnclosure.new(@client, name: 'Encl2')
+# Retrieve logical enclosure by name
+logical_enclosure_2 = OneviewSDK::LogicalEnclosure.new(@client, name: logical_enclosure['name'])
 logical_enclosure_2.retrieve!
-puts "Retrieved logical-enclosure '#{logical_enclosure_2[:name]}'.\n uri = '#{logical_enclosure_2[:uri]}'"
+puts "Retrieved logical-enclosure '#{logical_enclosure_2[:name]}' by name."
 
 
 # Get configuration script
-puts "Retrieved logical-enclosure '#{logical_enclosure[:name]}'.\n script = '#{logical_enclosure.get_script}'"
+orig_script = logical_enclosure.get_script
+puts "Retrieved logical-enclosure '#{logical_enclosure[:name]}' script\n  Content = '#{orig_script}'"
 
 # Set configuration script
 logical_enclosure.set_script('test')
-puts "Setting logical-enclosure '#{logical_enclosure[:name]}'.\n configuration script"
+puts "Setting logical-enclosure '#{logical_enclosure[:name]}' configuration script"
 
 # Get configuration script
-puts "Retrieved logical-enclosure '#{logical_enclosure[:name]}'.\n script = '#{logical_enclosure.get_script}'"
+puts "Retrieved logical-enclosure '#{logical_enclosure[:name]}' script\n  Content = '#{logical_enclosure.get_script}'"
 
 # Reset configuration script
-logical_enclosure.set_script('')
+logical_enclosure.set_script(orig_script)
+
 
 # Update from Group
-logical_enclosure.updateFromGroup
+logical_enclosure.update_from_group
 puts 'Logical enclosure updated'
+
 
 # Generate dump
 dump = {
@@ -34,5 +37,5 @@ dump = {
   encrypt: false,
   excludeApplianceDump: false
 }
-logical_enclosure.support_dumps(dump)
-puts "\nGenerated logical-enclosure '#{logical_enclosure[:name]}' dump"
+logical_enclosure.support_dump(dump)
+puts "\nGenerated dump for logical-enclosure '#{logical_enclosure[:name]}'."
--- lib/oneview-sdk-ruby/resource/logical_enclosure.rb
+++ lib/oneview-sdk-ruby/resource/logical_enclosure.rb
@@ -29,25 +29,12 @@ module OneviewSDK
       @data['type'] ||= 'LogicalEnclosure'
     end
 
-    # Create a logical enclosure in OneView
-    # @note Calls refresh method to set additional data
-    # @raise [RuntimeError] if the client is not set
-    # @raise [RuntimeError] if the resource creation fails
-    # @return [Resource] self
-    def create
-      ensure_client
-      response = @client.rest_post(self.class::BASE_URI, { 'body' => @data }, @api_version)
-      body = @client.response_handler(response)
-      set_all(body)
-      self
-    end
-
-    # Reapplies the appliance's configuration
+    # Reapplies the appliance's configuration on enclosures
     # @raise [RuntimeError] if the client is not set
     # @raise [RuntimeError] if the uri is not set
     # @raise [RuntimeError] if the reapply fails
-    # @return [Resource] self
-    def configuration
+    # @return [LogicalEnclosure] self
+    def reconfigure
       ensure_client && ensure_uri
       response = @client.rest_put("#{@data['uri']}/configuration", {}, @api_version)
       @client.response_handler(response)
@@ -59,11 +46,22 @@ module OneviewSDK
     # @raise [RuntimeError] if the uri is not set
     # @raise [RuntimeError] if the process fails
     # @return [Resource] self
-    def updateFromGroup
+    def update_from_group
       ensure_client && ensure_uri
       response = @client.rest_put("#{@data['uri']}/updateFromGroup", {}, @api_version)
       @client.response_handler(response)
       self
+    end
+
+    # Get the configuration script
+    # @raise [RuntimeError] if the client is not set
+    # @raise [RuntimeError] if the uri is not set
+    # @raise [RuntimeError] if retrieving fails
+    # @return [String] script
+    def get_script
+      ensure_client && ensure_uri
+      response = @client.rest_get("#{@data['uri']}/script", @api_version)
+      response.body
     end
 
     # Updates the configuration script for the logical enclosure
@@ -78,22 +76,12 @@ module OneviewSDK
       self
     end
 
-    # Get the configuration script
-    # @raise [RuntimeError] if the client is not set
-    # @raise [RuntimeError] if retrieving fails
-    # @return [String] script
-    def get_script
-      ensure_client
-      response = @client.rest_get("#{@data['uri']}/script", @api_version)
-      response.body
-    end
-
     # Generates a support dump for the logical enclosure
     # @raise [RuntimeError] if the client is not set
     # @raise [RuntimeError] if the uri is not set
     # @raise [RuntimeError] if the process fails when generating the support dump
     # @return [Resource] self
-    def support_dumps(options)
+    def support_dump(options)
       ensure_client && ensure_uri
       response = @client.rest_post("#{@data['uri']}/support-dumps", { 'body' => options }, @api_version)
       @client.wait_for(response.header['location'])
@@ -102,7 +90,7 @@ module OneviewSDK
 
     # Validate fabricType
     # @param [String] value DirectAttach, FabricAttach
-    # @raise [RuntimeError] if
+    # @raise [RuntimeError] if value is not 'DirectAttach' or 'FabricAttach'
     def validate_fabricType(value)
       fail 'Invalid fabric type' unless %w(DirectAttach FabricAttach).include?(value)
     end
