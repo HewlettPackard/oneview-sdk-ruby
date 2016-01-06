ef305dbb82a18b0dd47c71f5a5abf49bb0e17742
tmiotto <thiagomiottoam@gmail.com>
2016-01-06 14:29:57 -0200

    Changed example implmentation

--- examples/logical_interconnect.rb
+++ examples/logical_interconnect.rb
@@ -4,7 +4,15 @@ type = 'Logical Interconnect'
 
 # Explores functionalities of Logical Interconnects
 
+# Retrieves test enclosure to put the interconnects
+enclosure = OneviewSDK::Enclosure.new(@client, name: 'OneViewSDK_Test_Enclosure')
+enclosure.retrieve!
+puts "Sucessfully retrieved the enclosure #{enclosure[:name]}"
+
 log_int = OneviewSDK::LogicalInterconnect.new(@client, {})
 
-# Create interconnect in Bay 1 of Enclosure 1
-log_int.create_interconnect(1, 1)
+# Create interconnect in Bay 1 of Enclosure
+log_int.create(1, enclosure[:uri])
+puts "Created a new interconnect #{log_int[:name]}"
+
+log_int.delete
--- lib/oneview-sdk-ruby/resource/logical_interconnect.rb
+++ lib/oneview-sdk-ruby/resource/logical_interconnect.rb
@@ -19,20 +19,27 @@ module OneviewSDK
     # Creates an interconnect in the desired Bay in a specified enclosure
     # @param [Fixnum] Number of the bay to put the interconnect
     # @param [Fixnum] Number of the enclosure to insert the interconnect
-    def create_interconnect(bay_number, enclosure_number)
-      entry = 'locationEntries' => [
-        { 'relativeValue' => bay, 'type' => 'Bay' },
-        { 'relativeValue' => enclosure, 'type' => 'Enclosure' }
-      ]
-      ensure_client
+    def create(bay_number, enclosure_number)
+      entry =
+      {'locationEntries' => [
+        { 'value' => bay_number, 'type' => 'Bay' },
+        { 'value' => enclosure_number, 'type' => 'Enclosure' }
+      ]}
+      # ensure_client
       response = @client.rest_post(self.class::BASE_URI+'/locations/interconnects', { 'body' => entry }, @api_version)
       body = @client.response_handler(response)
+      set_all(body)
       self
     end
 
-    def delete_interconnect(bay_number, enclosure)
-      ensure_client
-      query_uri = self.class::BASE_URI+"/locations/interconnects?location=Enclosure:#{enclosure['uri']},Bay:#{bay_number}"
+    def delete
+      # ensure_client
+      int_location = @data['interconnectLocation']['locationEntries']
+      int_location.each do |entry|
+        enclosure_uri = entry['value'] if entry['type'] == 'Enclosure'
+        bay_number = entry['value'] if entry['type'] == 'Bay'
+      end
+      query_uri = self.class::BASE_URI+"/locations/interconnects?location=Enclosure:#{enclosure_uri},Bay:#{bay_number}"
       response = @client.rest_delete(query_uri, {}, @api_version)
       body = @client.response_handler(response)
       self
