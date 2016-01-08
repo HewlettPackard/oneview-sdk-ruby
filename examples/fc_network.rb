00d23d5af5ccdbba33e3d5891668edf8764e3b44
Ricardo Piantola <piantola@gmail.com>
2016-01-08 12:21:52 +0000

    Add fc-network update example

--- examples/fc_network.rb
+++ examples/fc_network.rb
@@ -16,21 +16,26 @@ puts "\nCreated fc-network '#{fc[:name]}' sucessfully.\n  uri = '#{fc[:uri]}'"
 # Find recently created network by name
 matches = OneviewSDK::FCNetwork.find_by(@client, name: fc[:name])
 fc2 = matches.first
-puts "\nFound ethernet-network by name: '#{fc2[:name]}'.\n  uri = '#{fc2[:uri]}'"
+puts "\nFound fc-network by name: '#{fc2[:name]}'.\n  uri = '#{fc2[:uri]}'"
 
 # Retrieve recently created network
 fc3 = OneviewSDK::FCNetwork.new(@client, name: fc[:name])
 fc3.retrieve!
-puts "\nRetrieved ethernet-network data by name: '#{fc3[:name]}'.\n  uri = '#{fc3[:uri]}'"
+puts "\nRetrieved fc-network data by name: '#{fc3[:name]}'.\n  uri = '#{fc3[:uri]}'"
 
-# Delete this network
-fc3.delete
-puts "\nSucessfully deleted fc-network '#{fc3[:name]}'."
-
+# Update fabricType from recently created network
+attribute = { autoLoginRedistribution: false }
+fc2.update(attribute)
+puts "\nUpdated fc-network: '#{fc[:name]}'.\n  uri = '#{fc2[:uri]}'"
+puts "with attribute: #{attribute}"
 
 # Example: List all fc networks with certain attributes
-attributes = { fabricType: 'FabricAttach' }
+attributes = { fabricType: 'fabricAttach' }
 puts "\n\nFC networks with #{attributes}"
 OneviewSDK::FCNetwork.find_by(@client, attributes).each do |network|
   puts "  #{network[:name]}"
 end
+
+# Delete this network
+fc3.delete
+puts "\nSucessfully deleted fc-network '#{fc3[:name]}'."
