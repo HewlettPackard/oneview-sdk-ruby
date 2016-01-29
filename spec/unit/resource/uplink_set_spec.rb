3a07d976c10c320a8c04709aafde55d648e3f236
henrique-diomede <henrique.diomede@hpe.com>
2016-01-29 14:54:15 -0200

    Fixed unit test

--- spec/unit/resource/uplink_set_spec.rb
+++ spec/unit/resource/uplink_set_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-RSpec.describe OneviewSDK::Uplink do
+RSpec.describe OneviewSDK::UplinkSet do
   include_context 'shared context'
 
   describe '#initialize' do
     it 'sets the defaults correctly' do
-      profile = OneviewSDK::Uplink.new(@client)
+      profile = OneviewSDK::UplinkSet.new(@client)
       expect(profile[:type]).to eq('uplink-setV3')
       expect(profile[:portConfigInfos]).to eq([])
       expect(profile[:networkUris]).to eq([])
@@ -18,7 +18,7 @@ RSpec.describe OneviewSDK::Uplink do
   describe 'validations' do
     context 'ethernetNetworkType' do
       it 'validates ethernetNetworkType' do
-        uplink = OneviewSDK::Uplink.new(@client)
+        uplink = OneviewSDK::UplinkSet.new(@client)
         values = %w(NotApplicable Tagged Tunnel Unknown Untagged)
         values.each do |value|
           uplink[:ethernetNetworkType] = value
@@ -27,14 +27,14 @@ RSpec.describe OneviewSDK::Uplink do
       end
 
       it 'Incorrect ethernetNetworkType' do
-        uplink = OneviewSDK::Uplink.new(@client)
+        uplink = OneviewSDK::UplinkSet.new(@client)
         expect { uplink[:ethernetNetworkType] = 'None' }.to raise_error(/Invalid ethernet network type/)
       end
     end
 
     context 'lacpTimer' do
       it 'validates lacpTimer' do
-        uplink = OneviewSDK::Uplink.new(@client)
+        uplink = OneviewSDK::UplinkSet.new(@client)
         values = %w(Long Short)
         values.each do |value|
           uplink[:lacpTimer] = value
@@ -43,14 +43,14 @@ RSpec.describe OneviewSDK::Uplink do
       end
 
       it 'Incorrect lacpTimer' do
-        uplink = OneviewSDK::Uplink.new(@client)
+        uplink = OneviewSDK::UplinkSet.new(@client)
         expect { uplink[:lacpTimer] = 'None' }.to raise_error(/Invalid lacp timer/)
       end
     end
 
     context 'manualLoginRedistributionState' do
       it 'validates manualLoginRedistributionState' do
-        uplink = OneviewSDK::Uplink.new(@client)
+        uplink = OneviewSDK::UplinkSet.new(@client)
         values = %w(Distributed Distributing DistributionFailed NotSupported Supported)
         values.each do |value|
           uplink[:manualLoginRedistributionState] = value
@@ -59,7 +59,7 @@ RSpec.describe OneviewSDK::Uplink do
       end
 
       it 'Incorrect manualLoginRedistributionState' do
-        uplink = OneviewSDK::Uplink.new(@client)
+        uplink = OneviewSDK::UplinkSet.new(@client)
         expect { uplink[:manualLoginRedistributionState] = 'None' }.to \
           raise_error(/Invalid manual login redistribution state/)
       end
@@ -67,7 +67,7 @@ RSpec.describe OneviewSDK::Uplink do
 
     context 'networkType' do
       it 'validates networkType' do
-        uplink = OneviewSDK::Uplink.new(@client)
+        uplink = OneviewSDK::UplinkSet.new(@client)
         values = %w(Ethernet FibreChannel)
         values.each do |value|
           uplink[:networkType] = value
@@ -76,14 +76,14 @@ RSpec.describe OneviewSDK::Uplink do
       end
 
       it 'Incorrect ethernetNetworkType' do
-        uplink = OneviewSDK::Uplink.new(@client)
+        uplink = OneviewSDK::UplinkSet.new(@client)
         expect { uplink[:networkType] = 'None' }.to raise_error(/Invalid network type/)
       end
     end
 
     context 'reachability' do
       it 'validates reachability' do
-        uplink = OneviewSDK::Uplink.new(@client)
+        uplink = OneviewSDK::UplinkSet.new(@client)
         values = %w(NotReachable Reachable RedundantlyReachable Unknown)
         values.each do |value|
           uplink[:reachability] = value
@@ -92,14 +92,14 @@ RSpec.describe OneviewSDK::Uplink do
       end
 
       it 'Incorrect reachability' do
-        uplink = OneviewSDK::Uplink.new(@client)
+        uplink = OneviewSDK::UplinkSet.new(@client)
         expect { uplink[:reachability] = 'None' }.to raise_error(/Invalid reachability/)
       end
     end
 
     context 'status' do
       it 'validates status' do
-        uplink = OneviewSDK::Uplink.new(@client)
+        uplink = OneviewSDK::UplinkSet.new(@client)
         values = %w(OK Disabled Warning Critical Unknown)
         values.each do |value|
           uplink[:status] = value
@@ -108,7 +108,7 @@ RSpec.describe OneviewSDK::Uplink do
       end
 
       it 'Incorrect status' do
-        uplink = OneviewSDK::Uplink.new(@client)
+        uplink = OneviewSDK::UplinkSet.new(@client)
         expect { uplink[:status] = 'None' }.to raise_error(/Invalid status/)
       end
     end
@@ -117,7 +117,7 @@ RSpec.describe OneviewSDK::Uplink do
   describe 'add elements' do
     context 'networks' do
       it 'Valid' do
-        uplink = OneviewSDK::Uplink.new(@client)
+        uplink = OneviewSDK::UplinkSet.new(@client)
         uplink.add_network(uri: '/rest/ethernet-networks')
         uplink.add_fcnetwork(uri: '/rest/fc-networks')
         uplink.add_fcoenetwork(uri: '/rest/fcoe-networks')
@@ -127,7 +127,7 @@ RSpec.describe OneviewSDK::Uplink do
       end
 
       it 'Incorrect' do
-        uplink = OneviewSDK::Uplink.new(@client)
+        uplink = OneviewSDK::UplinkSet.new(@client)
         expect { uplink.add_network({}) }.to raise_error(/Invalid object/)
         expect { uplink.add_fcnetwork({}) }.to raise_error(/Invalid object/)
         expect { uplink.add_fcoenetwork({}) }.to raise_error(/Invalid object/)
@@ -136,13 +136,13 @@ RSpec.describe OneviewSDK::Uplink do
 
     context 'logical interconnects' do
       it 'Valid' do
-        uplink = OneviewSDK::Uplink.new(@client)
+        uplink = OneviewSDK::UplinkSet.new(@client)
         uplink.set_logical_interconnect(uri: '/rest/logical-interconnects')
         expect(uplink[:logicalInterconnectUri]).to eq('/rest/logical-interconnects')
       end
 
       it 'Incorrect' do
-        uplink = OneviewSDK::Uplink.new(@client)
+        uplink = OneviewSDK::UplinkSet.new(@client)
         expect { uplink.set_logical_interconnect({}) }.to raise_error(/Invalid object/)
       end
     end
