eaf238f261cbc321c7b9f07d9179f15a081423b0
thiago-mio-amaral <thiago.mio.amaral@hpe.com>
2016-03-08 16:12:18 -0300

    Normalized ETH/FC/FCOE Networks and LIGs

--- spec/integration/resource/ethernet_network_spec.rb
+++ spec/integration/resource/ethernet_network_spec.rb
@@ -4,12 +4,14 @@ RSpec.describe OneviewSDK::EthernetNetwork, integration: true do
   include_context 'integration context'
 
   let(:file_path) { 'spec/support/fixtures/integration/ethernet_network.json' }
+  let(:resource_name) { 'EthernetNetwork_01' }
+  let(:updated_resource_name) { 'EthernetNetwork_01_UPDATED' }
 
   describe '#create' do
     it 'can create resources' do
       item = OneviewSDK::EthernetNetwork.from_file(@client, file_path)
       item.create
-      expect(item[:name]).to eq('OneViewSDK_Int_Ethernet_Network')
+      expect(item[:name]).to eq(resource_name)
       expect(item[:ethernetNetworkType]).to eq('Tagged')
       expect(item[:vlanId]).to eq(1001)
       expect(item[:purpose]).to eq('General')
@@ -20,9 +22,9 @@ RSpec.describe OneviewSDK::EthernetNetwork, integration: true do
 
   describe '#retrieve!' do
     it 'retrieves the resource' do
-      item = OneviewSDK::EthernetNetwork.new(@client, name: 'OneViewSDK_Int_Ethernet_Network')
+      item = OneviewSDK::EthernetNetwork.new(@client, name: resource_name)
       item.retrieve!
-      expect(item[:name]).to eq('OneViewSDK_Int_Ethernet_Network')
+      expect(item[:name]).to eq(resource_name)
       expect(item[:ethernetNetworkType]).to eq('Tagged')
       expect(item[:vlanId]).to eq(1001)
       expect(item[:purpose]).to eq('General')
@@ -33,30 +35,30 @@ RSpec.describe OneviewSDK::EthernetNetwork, integration: true do
 
   describe '#update' do
     it 'update OneViewSDK_Int_Ethernet_Network name' do
-      item = OneviewSDK::EthernetNetwork.new(@client, name: 'OneViewSDK_Int_Ethernet_Network')
+      item = OneviewSDK::EthernetNetwork.new(@client, name: resource_name)
       item.retrieve!
-      item.update(name: 'OneViewSDK_Int_Eth_Net')
+      item.update(name: updated_resource_name)
       item.refresh
-      expect(item[:name]).to eq('OneViewSDK_Int_Eth_Net')
+      expect(item[:name]).to eq(updated_resource_name)
     end
   end
 
   describe '#find_by' do
     it 'returns all resources when the hash is empty' do
       names = OneviewSDK::EthernetNetwork.find_by(@client, {}).map { |item| item[:name] }
-      expect(names).to include('OneViewSDK_Int_Eth_Net')
+      expect(names).to include(updated_resource_name)
     end
 
     it 'finds networks by multiple attributes' do
-      attrs = { name: 'OneViewSDK_Int_Eth_Net', vlanId: 1001, purpose: 'General' }
+      attrs = { name: updated_resource_name, vlanId: 1001, purpose: 'General' }
       names = OneviewSDK::EthernetNetwork.find_by(@client, attrs).map { |item| item[:name] }
-      expect(names).to include('OneViewSDK_Int_Eth_Net')
+      expect(names).to include(updated_resource_name)
     end
   end
 
   describe '#delete' do
     it 'deletes the resource' do
-      item = OneviewSDK::EthernetNetwork.new(@client, name: 'OneViewSDK_Int_Eth_Net')
+      item = OneviewSDK::EthernetNetwork.new(@client, name: updated_resource_name)
       item.retrieve!
       item.delete
     end
--- spec/integration/resource/fc_network_spec.rb
+++ spec/integration/resource/fc_network_spec.rb
@@ -4,12 +4,14 @@ RSpec.describe OneviewSDK::FCNetwork, integration: true do
   include_context 'integration context'
 
   let(:file_path) { 'spec/support/fixtures/integration/fc_network.json' }
+  let(:resource_name) { 'FCNetwork_01' }
+  let(:updated_resource_name) { 'FCNetwork_01_UPDATED' }
 
   describe '#create' do
     it 'can create resources' do
       item = OneviewSDK::FCNetwork.from_file(@client, file_path)
       item.create
-      expect(item[:name]).to eq('OneViewSDK Test FC Network')
+      expect(item[:name]).to eq(resource_name)
       expect(item[:autoLoginRedistribution]).to eq(true)
       expect(item[:connectionTemplateUri]).not_to eq(nil)
       expect(item[:fabricType]).to eq('FabricAttach')
@@ -18,9 +20,9 @@ RSpec.describe OneviewSDK::FCNetwork, integration: true do
 
   describe '#retrieve!' do
     it 'retrieves the resource' do
-      item = OneviewSDK::FCNetwork.new(@client, name: 'OneViewSDK Test FC Network')
+      item = OneviewSDK::FCNetwork.new(@client, name: resource_name)
       item.retrieve!
-      expect(item[:name]).to eq('OneViewSDK Test FC Network')
+      expect(item[:name]).to eq(resource_name)
       expect(item[:autoLoginRedistribution]).to eq(true)
       expect(item[:connectionTemplateUri]).not_to eq(nil)
       expect(item[:fabricType]).to eq('FabricAttach')
@@ -29,30 +31,30 @@ RSpec.describe OneviewSDK::FCNetwork, integration: true do
 
   describe '#update' do
     it 'update OneViewSDK Test FC Network name' do
-      item = OneviewSDK::FCNetwork.new(@client, name: 'OneViewSDK Test FC Network')
+      item = OneviewSDK::FCNetwork.new(@client, name: resource_name)
       item.retrieve!
-      item.update(name: 'OneViewSDK Test F Net')
+      item.update(name: updated_resource_name)
       item.refresh
-      expect(item[:name]).to eq('OneViewSDK Test F Net')
+      expect(item[:name]).to eq(updated_resource_name)
     end
   end
 
   describe '#find_by' do
     it 'returns all resources when the hash is empty' do
       names = OneviewSDK::FCNetwork.find_by(@client, {}).map { |item| item[:name] }
-      expect(names).to include('OneViewSDK Test F Net')
+      expect(names).to include(updated_resource_name)
     end
 
     it 'finds networks by multiple attributes' do
-      attrs = { name: 'OneViewSDK Test F Net', fabricType: 'FabricAttach' }
+      attrs = { name: updated_resource_name, fabricType: 'FabricAttach' }
       names = OneviewSDK::FCNetwork.find_by(@client, attrs).map { |item| item[:name] }
-      expect(names).to include('OneViewSDK Test F Net')
+      expect(names).to include(updated_resource_name)
     end
   end
 
   describe '#delete' do
     it 'deletes the resource' do
-      item = OneviewSDK::FCNetwork.new(@client, name: 'OneViewSDK Test F Net')
+      item = OneviewSDK::FCNetwork.new(@client, name: updated_resource_name)
       item.retrieve!
       expect { item.delete }.not_to raise_error
     end
