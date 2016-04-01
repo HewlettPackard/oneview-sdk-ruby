88fb4f7951c0067cc6f1d870088f017032aa3739
thiago-mio-amaral <thiago.mio.amaral@hpe.com>
2016-04-01 14:53:33 -0300

    Fixed standard for updates

--- spec/integration/resource/volume_template/update_spec.rb
+++ spec/integration/resource/volume_template/update_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe OneviewSDK::VolumeTemplate, integration: true, type: UPDATE do
 
   describe '#retrieve!' do
     it 'retrieves the resource' do
-      item = OneviewSDK::VolumeTemplate.new(@client, name: 'ONEVIEW_SDK_TEST VT1')
+      item = OneviewSDK::VolumeTemplate.new(@client, name: VOL_TEMP_NAME)
       item.retrieve!
-      expect(item[:name]).to eq('ONEVIEW_SDK_TEST VT1')
+      expect(item[:name]).to eq(v)
       expect(item[:description]).to eq('Volume Template')
       expect(item[:stateReason]).to eq('None')
       expect(item[:type]).to eq('StorageVolumeTemplateV3')
@@ -15,25 +15,30 @@ RSpec.describe OneviewSDK::VolumeTemplate, integration: true, type: UPDATE do
   end
 
   describe '#update' do
-    it 'update ONEVIEW_SDK_TEST VT1' do
-      item = OneviewSDK::VolumeTemplate.new(@client, name: 'ONEVIEW_SDK_TEST VT1')
+    it 'update volume name' do
+      item = OneviewSDK::VolumeTemplate.new(@client, name: VOL_TEMP_NAME)
       item.retrieve!
-      item.update(name: 'ONEVIEW_SDK_TEST VT2')
+
+      item.update(name: VOL_TEMP_NAME_UPDATED)
       item.refresh
-      expect(item[:name]).to eq('ONEVIEW_SDK_TEST VT2')
+      expect(item[:name]).to eq(VOL_TEMP_NAME_UPDATED)
+
+      item.update(name: VOL_TEMP_NAME)
+      item.refresh
+      expect(item[:name]).to eq(VOL_TEMP_NAME)
     end
   end
 
   describe '#find_by' do
     it 'returns all resources when the hash is empty' do
       names = OneviewSDK::VolumeTemplate.find_by(@client, {}).map { |item| item[:name] }
-      expect(names).to include('ONEVIEW_SDK_TEST VT2')
+      expect(names).to include(VOL_TEMP_NAME)
     end
 
     it 'finds networks by multiple attributes' do
-      attrs = { name: 'ONEVIEW_SDK_TEST VT2', type: 'StorageVolumeTemplateV3' }
+      attrs = { name: VOL_TEMP_NAME, type: 'StorageVolumeTemplateV3' }
       names = OneviewSDK::VolumeTemplate.find_by(@client, attrs).map { |item| item[:name] }
-      expect(names).to include('ONEVIEW_SDK_TEST VT2')
+      expect(names).to include(VOL_TEMP_NAME)
     end
   end
 end
