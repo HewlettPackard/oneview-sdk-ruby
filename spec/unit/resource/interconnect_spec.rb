23e5b7e30341a74fc30900216d955f0efa992dae
henrique-diomede <henrique.diomede@hpe.com>
2016-04-19 14:38:38 -0300

    Interconnect unit tests fix

--- spec/unit/resource/interconnect_spec.rb
+++ spec/unit/resource/interconnect_spec.rb
@@ -9,15 +9,15 @@ RSpec.describe OneviewSDK::Interconnect do
     end
 
     it 'does not allow the create action' do
-      expect { @item.create }.to raise_error(/not available for this resource/)
+      expect { @item.create }.to raise_error(/The method #create is unavailable for this resource/)
     end
 
     it 'does not allow the update action' do
-      expect { @item.update }.to raise_error(/not available for this resource/)
+      expect { @item.update }.to raise_error(/The method #update is unavailable for this resource/)
     end
 
     it 'does not allow the save action' do
-      expect { @item.save }.to raise_error(/not available for this resource/)
+      expect { @item.save }.to raise_error(/The method #save is unavailable for this resource/)
     end
   end
 end
