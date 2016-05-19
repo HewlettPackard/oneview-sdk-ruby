49e6823cf67fd3dced9d07bd38e35a38cb0a524c
henrique-diomede <henrique.diomede@hpe.com>
2016-05-19 11:32:52 -0300

    Connection template implemented

---
+++ lib/oneview-sdk/resource/connection_template.rb
@@ -0,0 +1,30 @@
+module OneviewSDK
+  # Connection template resource implementation
+  class ConnectionTemplate < Resource
+    BASE_URI = '/rest/connection-templates'.freeze
+
+    def initialize(client, params = {}, api_ver = nil)
+      super
+      # Default values:
+      @data['bandwidth'] ||= {}
+      @data['type'] ||= 'connection-template'
+    end
+
+    def create
+      unavailable_method
+    end
+
+    def delete
+      unavailable_method
+    end
+
+    # Get the default network connection template
+    # @param [OneviewSDK::Client] client Oneview client
+    # @return [OneviewSDK::ConnectionTemplate] Connection template
+    def self.get_default(client)
+      response = client.rest_get(BASE_URI + '/defaultConnectionTemplate')
+      OneviewSDK::ConnectionTemplate.new(client, client.response_handler(response))
+    end
+
+  end
+end
