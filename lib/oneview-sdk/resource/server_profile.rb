ed6d18d07d71d8aefe2b76ead3567cac7e70ab43
thiago-mio-amaral <thiago.mio.amaral@hpe.com>
2016-05-19 10:00:59 -0300

    Added few basic methods

--- lib/oneview-sdk/resource/server_profile.rb
+++ lib/oneview-sdk/resource/server_profile.rb
@@ -9,7 +9,74 @@ module OneviewSDK
       @data['type'] ||= 'ServerProfileV5'
     end
 
-    # Get available server hardware for this template
+    def create
+    end
+
+    # Sets the Server Hardware for the resource
+    # @param [OneviewSDK::ServerHardware] server_hardware Server Hardware resource
+    def set_server_hardware(server_hardware)
+      self['serverHardwareUri'] = server_hardware['uri'] if server_hardware['uri'] || server_hardware.retrieve!
+      fail "Resource #{server_hardware['name']} could not be found!" unless server_hardware['uri']
+    end
+
+    # Sets the Server Hardware Type for the resource
+    # @param [OneviewSDK::ServerHardwareType] server_hardware_type Type of the desired Server Hardware
+    def set_server_hardware_type(server_hardware_type)
+      self['serverHardwareTypeUri'] = server_hardware_type['uri'] if server_hardware_type['uri'] || server_hardware_type.retrieve!
+      fail "Resource #{server_hardware_type['name']} could not be found!" unless server_hardware_type['uri']
+    end
+
+    # Sets the Enclosure Group for the resource
+    # @param [OneviewSDK::EnclosureGroup] enclosure_group Enclosure Group that the Server is a member
+    def set_enclosure_group(enclosure_group)
+      self['enclosureGroupUri'] = enclosure_group['uri'] if enclosure_group['uri'] || enclosure_group.retrieve!
+      fail "Resource #{enclosure_group['name']} could not be found!" unless enclosure_group['uri']
+    end
+
+    # Get all the available Ethernet and FC Networks
+    # @param [OneviewSDK::Client] client Appliance client
+    # @return [Hash{String=>Array<OneviewSDK::EthernetNetwork>,Array<OneviewSDK::FCNetwork>}] 
+    #   A hash containing the lists of Ethernet Networks and FC Networks
+    # @option bununu [String] :ethernet_networks The list of Ethernet Networks
+    # @option bununu [String] :fc_networks The list of FC Networks
+    def self.get_available_networks(client)
+      response = client.rest_get("#{BASE_URI}/available-networks")
+      ethernet_networks = response.body['ethernetNetworks'].select { |info| OneviewSDK::EthernetNetwork.find_by(name: info['name']).first }
+      fc_networks = response.body['fcNetworks'].select { |info| OneviewSDK::FCNetwork.find_by(name: info['name']).first }
+      return {
+        'ethernet_networks' => ethernet_networks,
+        'fc_networks' => fc_networks
+      }
+    end
+
+    def self.get_available_servers
+    end
+
+    def self.get_available_storage_system
+    end
+
+    def self.get_available_storage_systems
+    end
+
+    def self.get_available_targets
+    end
+
+    def self.get_profile_ports
+    end
+
+    def get_compliance_preview
+    end
+
+    def get_messages
+    end
+
+    def get_transformation
+    end
+
+    def compliance
+    end
+
+    # Get available server hardware
     # @return [Array<OneviewSDK::ServerHardware>] Array of ServerHardware resources that matches this
     #   profile's server hardware type and enclosure group and who's state is 'NoProfileApplied'
     def available_hardware
@@ -26,12 +93,5 @@ module OneviewSDK
       raise "Failed to get available hardware. Message: #{e.message}"
     end
 
-    def validate_serverProfileTemplateUri(*)
-      fail "Templates only exist on api version >= 200. Resource version: #{@api_version}" if @api_version < 200
-    end
-
-    def validate_templateCompliance(*)
-      fail "Templates only exist on api version >= 200. Resource version: #{@api_version}" if @api_version < 200
-    end
   end
 end
