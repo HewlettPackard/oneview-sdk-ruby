50e48130677ee4801f6f1ea9007793c36f535436
thiago-mio-amaral <thiago.mio.amaral@hpe.com>
2016-06-23 17:48:34 -0300

    Finished implementation of Server Profile Templates

--- lib/oneview-sdk/resource/server_profile_template.rb
+++ lib/oneview-sdk/resource/server_profile_template.rb
@@ -20,15 +20,97 @@ module OneviewSDK
       @data['type'] ||= 'ServerProfileTemplateV1'
     end
 
+    # Sets the Server Hardware Type for the Server Profile Template
+    # @param [OneviewSDK::ServerHardwareType] server_hardware_type Type of the desired Server Hardware
+    def set_server_hardware_type(server_hardware_type)
+      self['serverHardwareTypeUri'] = server_hardware_type['uri'] if server_hardware_type['uri'] || server_hardware_type.retrieve!
+      fail "Resource #{server_hardware_type['name']} could not be found!" unless server_hardware_type['uri']
+    end
+
+    # Sets the Enclosure Group for the Server Profile Template
+    # @param [OneviewSDK::EnclosureGroup] enclosure_group Enclosure Group that the Server is a member
+    def set_enclosure_group(enclosure_group)
+      self['enclosureGroupUri'] = enclosure_group['uri'] if enclosure_group['uri'] || enclosure_group.retrieve!
+      fail "Resource #{enclosure_group['name']} could not be found!" unless enclosure_group['uri']
+    end
+
+    # Sets the Firmware Driver for the current Server Profile Template
+    # @param [OneviewSDK::FirmwareDriver] firmware Firmware Driver to be associated with the resource
+    # @param [Hash<String,Object>] firmware_options Firmware Driver options
+    # @option firmware_options [Boolean] 'manageFirmware' Indicates that the server firmware is configured using the server profile.
+    #   Value can be 'true' or 'false'.
+    # @option firmware_options [Boolean] 'forceInstallFirmware' Force installation of firmware even if same or newer version is installed.
+    #   Downgrading the firmware can result in the installation of unsupported firmware and cause server hardware to cease operation.
+    #   Value can be 'true' or 'false'.
+    # @option firmware_options [String] 'firmwareInstallType' Specifies the way a Service Pack for ProLiant (SPP) is installed.
+    #   This field is used if the 'manageFirmware' field is true.
+    #   Values are 'FirmwareAndOSDrivers', 'FirmwareOnly', and 'FirmwareOnlyOfflineMode'.
+    def set_firmware_driver(firmware, firmware_options = {})
+      firmware_options['firmwareBaselineUri'] = firmware['uri'] if firmware['uri'] || firmware.retrieve!
+      self['firmware'] = firmware_options
+    end
+
+    # Add connection entry to Server profile template
+    # @param [OneviewSDK::EthernetNetwork,OneviewSDK::FCNetwork] network Network associated with the connection
+    # @param [Hash<String,String>] connection_options Hash containing the configuration of the connection
+    # @option connection_options [Integer] 'allocatedMbps' The transmit throughput (mbps) currently allocated to
+    #   this connection. When Fibre Channel connections are set to Auto for requested bandwidth, the value can be set to -2000
+    #   to indicate that the actual value is unknown until OneView is able to negotiate the actual speed.
+    # @option connection_options [Integer] 'allocatedVFs' The number of virtual functions allocated to this connection. This value will be null.
+    # @option connection_options [Hash] 'boot' indicates that the server will attempt to boot from this connection.
+    #   This object can only be specified if "boot.manageBoot" is set to 'true'
+    # @option connection_options [String] 'deploymentStatus' The deployment status of the connection.
+    #   The value can be 'Undefined', 'Reserved', or 'Deployed'.
+    # @option connection_options [String] 'functionType' Type of function required for the connection.
+    #   functionType cannot be modified after the connection is created.
+    # @option connection_options [String] 'mac' The MAC address that is currently programmed on the FlexNic.
+    # @option connection_options [String] 'macType' Specifies the type of MAC address to be programmed into the IO Devices.
+    #   The value can be 'Virtual', 'Physical' or 'UserDefined'.
+    # @option connection_options [String] 'maximumMbps' Maximum transmit throughput (mbps) allowed on this connection.
+    #   The value is limited by the maximum throughput of the network link and maximumBandwidth of the selected network (networkUri).
+    #   For Fibre Channel connections, the value is limited to the same value as the allocatedMbps.
+    # @option connection_options [String] 'name' A string used to identify the respective connection.
+    #   The connection name is case insensitive, limited to 63 characters and must be unique within the profile.
+    # @option connection_options [String] 'portId' Identifies the port (FlexNIC) used for this connection.
+    # @option connection_options [String] 'requestedMbps' The transmit throughput (mbps) that should be allocated to this connection.
+    # @option connection_options [String] 'requestedVFs' This value can be "Auto" or 0.
+    # @option connection_options [String] 'wwnn' The node WWN address that is currently programmed on the FlexNic.
+    # @option connection_options [String] 'wwpn' The port WWN address that is currently programmed on the FlexNic.
+    # @option connection_options [String] 'wwpnType' Specifies the type of WWN address to be porgrammed on the FlexNIC.
+    #   The value can be 'Virtual', 'Physical' or 'UserDefined'.
+    def add_connection(network, connection_options = {})
+      self['connections'] = [] unless self['connections']
+      connection_options['id'] = 0 # Letting OneView treat the ID registering
+      connection_options['networkUri'] = network['uri'] if network['uri'] || network.retrieve!
+      self['connections'] << connection_options
+    end
+
+    # Remove connection entry in Server profile template
+    # @param [String] connection_name Name of the connection
+    # @return Returns the connection hash if found, otherwise returns nil
+    def remove_connection(connection_name)
+      desired_connection = nil
+      return desired_connection unless self['connections']
+      self['connections'].each do |con|
+        desired_connection = self['connections'].delete(con) if con['name'] == connection_name
+      end
+      desired_connection
+    end
+
     # Create ServerProfile using this template
     # @param [String] name Name of new server profile
-    # @return [ServerProfile] New server profile from template.
+    # @return [OneviewSDK::ServerProfile] New server profile from template.
     #   Temporary object only; call .create to actually create resource on OneView.
     def new_profile(name = nil)
       ensure_client && ensure_uri
       options = @client.rest_get("#{@data['uri']}/new-profile")
-      profile = OneviewSDK::ServerProfile.new(@client, options)
-      profile[:name] = name if name && name.size > 0
+      body = @client.response_handler(options)
+      profile = OneviewSDK::ServerProfile.new(@client, body)
+      profile['name'] = if name && name.size > 0
+                          name
+                        else
+                          "Server_Profile_created_from_#{self['name']}"
+                        end
       profile
     end
   end
--- spec/unit/resource/server_profile_template_spec.rb
+++ spec/unit/resource/server_profile_template_spec.rb
@@ -3,24 +3,174 @@ require 'spec_helper'
 RSpec.describe OneviewSDK::ServerProfileTemplate do
   include_context 'shared context'
 
+  before(:each) do
+    @item = OneviewSDK::ServerProfileTemplate.new(@client, name: 'server_profile_template')
+  end
+
   describe '#initialize' do
     it 'sets the type correctly' do
-      template = OneviewSDK::ServerProfileTemplate.new(@client)
-      expect(template[:type]).to eq('ServerProfileTemplateV1')
+      expect(@item[:type]).to eq('ServerProfileTemplateV1')
+    end
+  end
+
+  describe '#set_server_hardware_type' do
+    before :each do
+      @server_hardware_type = OneviewSDK::ServerHardwareType.new(@client, name: 'server_hardware_type')
+      @server_hardware_type_uri = '/rest/fake/server-hardware-types/test'
+    end
+
+    it 'will set the serverHardwareTypeUri correctly' do
+      @server_hardware_type['uri'] = @server_hardware_type_uri
+      @item.set_server_hardware_type(@server_hardware_type)
+      expect(@item['serverHardwareTypeUri']).to eq(@server_hardware_type_uri)
+    end
+
+    it 'will retrieve and set the serverHardwareTypeUri correctly' do
+      expect(@client).to receive(:rest_get).with('/rest/server-hardware-types')
+        .and_return(FakeResponse.new(members: [
+            { name: 'server_hardware_type', uri: @server_hardware_type_uri },
+            { name: 'wrong_server_hardware_type', uri: 'wrong_uri' }
+          ]))
+      @item.set_server_hardware_type(@server_hardware_type)
+      expect(@item['serverHardwareTypeUri']).to eq(@server_hardware_type_uri)
+    end
+
+    it 'will fail to put serverHardwareTypeUri since the resource does not exists' do
+      expect(@client).to receive(:rest_get).with('/rest/server-hardware-types')
+        .and_return(FakeResponse.new(members: [
+            { name: 'wrong_server_hardware_type', uri: 'wrong_uri' }
+          ]))
+      expect { @item.set_server_hardware_type(@server_hardware_type) }.to raise_error(/could not be found/)
+    end
+  end
+
+  describe '#set_enclosure_group' do
+    before :each do
+      @enclosure_group = OneviewSDK::EnclosureGroup.new(@client, name: 'enclosure_group')
+      @enclosure_group_uri = '/rest/fake/enclosure-groups/test'
+    end
+
+    it 'will set the enclosureGroupUri correctly' do
+      @enclosure_group['uri'] = @enclosure_group_uri
+      @item.set_enclosure_group(@enclosure_group)
+      expect(@item['enclosureGroupUri']).to eq(@enclosure_group_uri)
+    end
+
+    it 'will retrieve and set the enclosureGroupUri correctly' do
+      expect(@client).to receive(:rest_get).with('/rest/enclosure-groups')
+        .and_return(FakeResponse.new(members: [
+            { name: 'enclosure_group', uri: @enclosure_group_uri },
+            { name: 'wrong_enclosure_group', uri: 'wrong_uri' }
+          ]))
+      @item.set_enclosure_group(@enclosure_group)
+      expect(@item['enclosureGroupUri']).to eq(@enclosure_group_uri)
+    end
+
+    it 'will fail to put enclosureGroupUri since the resource does not exists' do
+      expect(@client).to receive(:rest_get).with('/rest/enclosure-groups')
+        .and_return(FakeResponse.new(members: [
+            { name: 'wrong_enclosure_group', uri: 'wrong_uri' }
+          ]))
+      expect { @item.set_enclosure_group(@enclosure_group) }.to raise_error(/could not be found/)
+    end
+  end
+
+  describe '#set_firmware_driver' do
+    before :each do
+      @firmware_uri = '/rest/fake/firmware-drivers/unit'
+      @firmware = OneviewSDK::FirmwareDriver.new(@client, name: 'unit_firmware_driver', uri: @firmware_uri)
+    end
+
+    it 'will set the FirmwareDriver with options correctly' do
+      @item.set_firmware_driver(@firmware, 'manageFirmware' => false)
+      expect(@item['firmware']['firmwareBaselineUri']).to eq(@firmware_uri)
+      expect(@item['firmware']['manageFirmware']).to eq(false)
+    end
+  end
+
+  describe '#add_connection' do
+    before :each do
+      @item['connections'] = []
+      @network = OneviewSDK::EthernetNetwork.new(@client, name: 'unit_ethernet_network', uri: 'rest/fake/ethernet-networks/unit')
+    end
+
+    it 'adds simple connection' do
+      expect { @item.add_connection(@network, 'name' => 'unit_net') }.to_not raise_error
+      expect(@item['connections']).to be
+      expect(@item['connections'].first['id']).to eq(0)
+      expect(@item['connections'].first['networkUri']).to eq('rest/fake/ethernet-networks/unit')
+    end
+
+    it 'adds multiple connections' do
+      base_uri = @network['uri']
+      1.upto(4) do |count|
+        @network['uri'] = "#{@network['uri']}_#{count}"
+        expect { @item.add_connection(@network, 'name' => "unit_net_#{count}") }.to_not raise_error
+        @network['uri'] = base_uri
+      end
+      @item['connections'].each do |connection|
+        expect(connection['id']).to eq(0)
+        expect(connection['name']).to match(/unit_net_/)
+      end
+    end
+
+    describe '#remove_connection' do
+      before :each do
+        @item['connections'] = []
+        @network = OneviewSDK::EthernetNetwork.new(@client, name: 'unit_ethernet_network', uri: 'rest/fake/ethernet-networks/unit')
+        base_uri = @network['uri']
+        1.upto(5) do |count|
+          @network['uri'] = "#{@network['uri']}_#{count}"
+          @item.add_connection(@network, 'name' => "unit_con_#{count}")
+          @network['uri'] = base_uri
+        end
+      end
+
+      it 'removes a connection' do
+        removed_connection = @item.remove_connection('unit_con_2')
+        expect(removed_connection['name']).to eq('unit_con_2')
+        expect(removed_connection['networkUri']).to eq("#{@network['uri']}_2")
+        @item['connections'].each do |connection|
+          expect(connection['name']).to_not eq(removed_connection['name'])
+        end
+      end
+
+      it 'removes all connections' do
+        1.upto(5) do |count|
+          removed_connection = @item.remove_connection("unit_con_#{count}")
+          expect(removed_connection['name']).to eq("unit_con_#{count}")
+          expect(removed_connection['networkUri']).to eq("#{@network['uri']}_#{count}")
+          @item['connections'].each do |connection|
+            expect(connection['name']).to_not eq(removed_connection['name'])
+          end
+        end
+        expect(@item['connections']).to be_empty
+      end
+
+      it 'returns nil if no connection set' do
+        @item.data.delete('connections')
+        expect(@item.remove_connection('fake')).not_to be
+      end
+
+      it 'returns nil if connection does not exists' do
+        expect(@item.remove_connection('fake')).not_to be
+      end
     end
   end
 
   describe '#new_profile' do
     it 'returns a profile' do
-      allow_any_instance_of(OneviewSDK::Client).to receive(:rest_get).and_return(name: 'NewProfile')
+      allow_any_instance_of(OneviewSDK::Client).to receive(:rest_get).and_return(FakeResponse.new(name: 'NewProfile'))
       expect(@client).to receive(:rest_get).with('/rest/server-profile-templates/fake/new-profile')
-      template = OneviewSDK::ServerProfileTemplate.new(@client, uri: '/rest/server-profile-templates/fake')
+      template = OneviewSDK::ServerProfileTemplate.new(@client, name: 'unit_server_profile_template')
+      template['uri'] = '/rest/server-profile-templates/fake'
       profile = template.new_profile
       expect(profile.class).to eq(OneviewSDK::ServerProfile)
+      expect(profile['name']).to eq("Server_profile_created_from_#{template['name']}")
     end
 
     it 'can set the name of a new profile' do
-      allow_any_instance_of(OneviewSDK::Client).to receive(:rest_get).and_return(name: 'NewProfile')
+      allow_any_instance_of(OneviewSDK::Client).to receive(:rest_get).and_return(FakeResponse.new(name: 'NewProfile'))
       template = OneviewSDK::ServerProfileTemplate.new(@client, uri: '/rest/server-profile-templates/fake')
       profile = template.new_profile('NewName')
       expect(profile[:name]).to eq('NewName')
