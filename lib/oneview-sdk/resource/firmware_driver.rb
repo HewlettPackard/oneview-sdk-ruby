bfb47fdc02080310382117a3c288142c18d2b51b
henrique-diomede <henrique.diomede@hpe.com>
2016-07-13 13:20:29 -0300

    Implemented firmware driver

--- lib/oneview-sdk/resource/firmware_driver.rb
+++ lib/oneview-sdk/resource/firmware_driver.rb
@@ -14,19 +14,8 @@ module OneviewSDK
   class FirmwareDriver < Resource
     BASE_URI = '/rest/firmware-drivers'.freeze
 
-    def initialize(client, params = {}, api_ver = nil)
-      super
-      # Default values
-      @data['type'] ||= 'firmware-baselines'
-    end
-
-    def create
-      unavailable_method
-    end
-
     def update
       unavailable_method
     end
-
   end
 end
