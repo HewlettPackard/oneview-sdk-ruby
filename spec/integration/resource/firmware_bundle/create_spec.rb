1a82d8f3c16e6e8294cdb5308734f04b7902a1b6
henrique-diomede <henrique.diomede@hpe.com>
2016-07-18 15:17:14 -0300

    firmware bundle path for integration tests

--- spec/integration/resource/firmware_bundle/create_spec.rb
+++ spec/integration/resource/firmware_bundle/create_spec.rb
@@ -15,9 +15,11 @@ klass = OneviewSDK::FirmwareBundle
 RSpec.describe klass, integration: true, type: CREATE, sequence: seq(klass) do
   include_context 'integration context'
 
+  let(:bundle_path) { 'spec/support/hp-firmware-a1b08f8a6b-HPGH-1.1.i386.rpm' }
+
   describe '#self.upload' do
     it 'Upload hotfix' do
-      item = OneviewSDK::FirmwareBundle.upload($client, 'spec/support/hp-firmware-a1b08f8a6b-HPGH-1.1.i386.rpm')
+      item = OneviewSDK::FirmwareBundle.upload($client, bundle_path)
       expect(item['uri']).to be
     end
   end
