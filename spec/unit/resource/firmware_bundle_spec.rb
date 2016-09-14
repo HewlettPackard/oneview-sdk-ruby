e58dd4d80791a744b9426b4a50733af6b8c444c7
henrique-diomede <henrique.diomede@hpe.com>
2016-09-14 15:53:57 -0300

    increased coverage

--- spec/unit/resource/firmware_bundle_spec.rb
+++ spec/unit/resource/firmware_bundle_spec.rb
@@ -9,12 +9,13 @@ RSpec.describe OneviewSDK::FirmwareBundle do
     end
 
     it 'returns a FirmwareDriver resource' do
-      allow_any_instance_of(Net::HTTP).to receive(:request).and_return(FakeResponse.new({}))
+      allow_any_instance_of(Net::HTTP).to receive(:request).and_return(FakeResponse.new({}, 200))
+      allow_any_instance_of(Net::HTTP).to receive(:connect).and_return(true)
       allow(@client).to receive(:response_handler).and_return(uri: '/rest/firmware-drivers/f1')
-      allow(File).to receive('file?').and_return(true)
+      allow(File).to receive(:file?).and_return(true)
+      allow(File).to receive(:open).with('file.tar').and_yield('FAKE FILE CONTENT')
       allow(UploadIO).to receive(:new).and_return('FAKE FILE CONTENT')
-      expect(OneviewSDK::FirmwareBundle).to receive(:add).and_return(OneviewSDK::FirmwareDriver)
-      OneviewSDK::FirmwareBundle.add(@client, 'file.tar')
+      expect(OneviewSDK::FirmwareBundle.add(@client, 'file.tar').class).to eq(OneviewSDK::FirmwareDriver)
     end
   end
 end
