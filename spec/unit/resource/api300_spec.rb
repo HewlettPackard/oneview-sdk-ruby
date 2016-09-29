57250f4eb5a819c59e7a6b6059d2809024da9a70
Ana Raquel Campesan <ana.campesan@hpe.com>
2016-09-29 10:32:49 -0400

    added some module unit tests

--- spec/unit/resource/api300_spec.rb
+++ spec/unit/resource/api300_spec.rb
@@ -7,6 +7,15 @@ RSpec.describe OneviewSDK::API300 do
     ['C7000', 'Thunderbird'].each { |v| expect(versions).to include(v) }
   end
 
+  it 'returns a valid API300 version' do
+    ['C7000', 'Thunderbird'].each { |v| expect{ OneviewSDK::API300::const_get(v) }.not_to raise_error }
+  end
+
+  it 'raises an error when an invalid API300 version is called' do
+    expect{ OneviewSDK::API300::C6000 }.to raise_error(NameError,
+                                                       'The C6000 method or resource does not exist for OneView API300 version C7000.')
+  end
+
   it 'has a default api version' do
     expect(described_class::DEFAULT_API300_VERSION).to eq('C7000')
   end
