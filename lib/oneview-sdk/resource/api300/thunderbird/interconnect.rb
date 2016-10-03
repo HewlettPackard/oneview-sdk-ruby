15d0fae932e3ca7556e3d7eda5a4853efc5421e8
fgbulsoni <fgbulsoni@hpe.com>
2016-10-03 16:34:34 -0300

    Added ILT resource endpoints and unit tests

--- lib/oneview-sdk/resource/api300/thunderbird/interconnect.rb
+++ lib/oneview-sdk/resource/api300/thunderbird/interconnect.rb
@@ -15,6 +15,27 @@ module OneviewSDK
   module API300
     module Thunderbird
       class Interconnect < OneviewSDK::API200::Interconnect
+        LINK_TOPOLOGY_URI = '/rest/interconnect-link-topologies'.freeze
+
+
+        # Retrieves the interconnect link topologies
+        # @param [OneviewSDK::Client] client The client object for the OneView appliance
+        # @return [Array] All the Interconnect Link Topologies
+        def self.get_link_topologies(client)
+          response = client.rest_get(LINK_TOPOLOGY_URI)
+          response = client.response_handler(response)
+          response['members']
+        end
+
+        # Retrieves the interconnect link topology with the name
+        # @param [OneviewSDK::Client] client The client object for the OneView appliance
+        # @param [String] name Switch type name
+        # @return [Array] Switch type
+        def self.get_link_topology(client, name)
+          results = get_link_topologies(client)
+          results.find { |interconnect_link_topology| interconnect_link_topology['name'] == name }
+        end
+
       end
     end
   end
