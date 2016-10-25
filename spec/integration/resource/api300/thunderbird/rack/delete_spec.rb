4a59f46b5c3b186dcc84ec822af07c237d2e1e83
Miguel Franklin <miguel.mariano-filho@hpe.com>
2016-10-25 17:21:17 -0300

    adding integration tests for api300 for both c7000 and thunderbird

---
+++ spec/integration/resource/api300/thunderbird/rack/delete_spec.rb
@@ -0,0 +1,31 @@
+# (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+require 'spec_helper'
+
+klass = OneviewSDK::API300::Thunderbird::Rack
+RSpec.describe klass, integration: true, type: DELETE, sequence: rseq(klass) do
+  include_context 'integration api300 context'
+
+  describe '#remove' do
+    it 'Rack_1' do
+      item = klass.new($client_300, name: RACK1_NAME)
+      item.retrieve!
+      expect { item.remove }.not_to raise_error
+    end
+
+    it 'Rack_2' do
+      item = klass.new($client_300, name: RACK2_NAME)
+      item.retrieve!
+      expect { item.remove }.not_to raise_error
+    end
+  end
+end
