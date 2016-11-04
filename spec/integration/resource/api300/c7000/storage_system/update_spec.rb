bd0a01971e05a4603e48e85689a27a7f537f6aba
Miguel Franklin <miguel.mariano-filho@hpe.com>
2016-11-04 14:40:03 -0300

    adding tests for c7000

--- spec/integration/resource/api300/c7000/storage_system/update_spec.rb
+++ spec/integration/resource/api300/c7000/storage_system/update_spec.rb
@@ -4,5 +4,21 @@ klass = OneviewSDK::API300::C7000::StorageSystem
 RSpec.describe klass, integration: true, type: UPDATE do
   include_context 'integration api300 context'
 
-  # update doesn't work
+  describe '#update' do
+    it '#updating fc_network unmanaged ports' do
+      storage_system = klass.new($client_300, 'credentials' => {})
+      storage_system['credentials']['ip_hostname'] = $secrets['storage_system1_ip']
+      storage_system.retrieve!
+
+      fc_network = OneviewSDK::API300::Thunderbird::FCNetwork.find_by($client_300, {}).first
+
+      storage_system.data['unmanagedPorts'].first['expectedNetworkUri'] = fc_network.data['uri']
+      storage_system.update
+      new_item = klass.new($client_300, 'credentials' => {})
+      new_item['credentials']['ip_hostname'] = $secrets['storage_system1_ip']
+      new_item.retrieve!
+      list = new_item.data['unmanagedPorts'].select { |a| a['expectedNetworkUri'] == fc_network['uri'] }
+      expect(list.empty?).to be false
+    end
+  end
 end
