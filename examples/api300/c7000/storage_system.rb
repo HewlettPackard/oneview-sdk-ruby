42fc049a1207c57573070264f0cd9222677198ee
Miguel Franklin <miguel.mariano-filho@hpe.com>
2016-11-04 17:38:04 -0300

    fixing client path

--- examples/api300/c7000/storage_system.rb
+++ examples/api300/c7000/storage_system.rb
@@ -9,7 +9,7 @@
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-require_relative '../_client' # Gives access to @client
+require_relative '../../_client' # Gives access to @client
 
 # Example: Create a storage system
 # NOTE: This will create a storage system, then delete it.
