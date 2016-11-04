128675eef6a6b0304f0b7de13eb2361a66d25737
Miguel Franklin <miguel.mariano-filho@hpe.com>
2016-11-04 13:38:11 -0300

    adding update_spec to storage system

--- spec/integration/resource/api300/thunderbird/storage_system/update_spec.rb
+++ spec/integration/resource/api300/thunderbird/storage_system/update_spec.rb
@@ -4,5 +4,15 @@ klass = OneviewSDK::API300::Thunderbird::StorageSystem
 RSpec.describe klass, integration: true, type: UPDATE do
   include_context 'integration api300 context'
 
-  # update doesn't work
+  before :all do
+    @item = klass.get_all($client_300).first
+    @fc_network = OneviewSDK::API300::Thunderbird::FCNetwork.find_by($client_300, {}).first
+  end
+
+  it "#updating fc_network unmanaged ports" do
+    @item.data["unmanagedPorts"].at(2)["expectedNetworkUri"] = @fc_network.data['uri']
+    @item.update
+    new_item = klass.get_all($client_300).first
+    expect(new_item['unmanagedPorts'].at(2)["expectedNetworkUri"]).to eq @fc_network.data['uri']
+  end
 end
