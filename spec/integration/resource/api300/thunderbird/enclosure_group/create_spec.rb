20288dc8bf925be6525aff29725d4ab4e0e141b0
Ricardo Galeno <ricardo.souza-filho@hpe.com>
2016-12-12 15:55:18 -0300

    Test of deleting EnclosureGroup more readable

--- spec/integration/resource/api300/thunderbird/enclosure_group/create_spec.rb
+++ spec/integration/resource/api300/thunderbird/enclosure_group/create_spec.rb
@@ -42,8 +42,11 @@ RSpec.describe klass, integration: true, type: CREATE, sequence: seq(klass) do
       item_2.create
       expect(item_2['name']).to eq(ENC_GROUP2_NAME)
       item_2['interconnectBayMappings'].each do |bay|
-        expect(bay['logicalInterconnectGroupUri']).to eq(lig['uri']) if [3, 6].include?(bay['interconnectBay'])
-        expect(bay['logicalInterconnectGroupUri']).to_not be unless [3, 6].include?(bay['interconnectBay'])
+        if bay['interconnectBay'] == 3 || bay['interconnectBay'] == 6
+          expect(bay['logicalInterconnectGroupUri']).to eq(lig['uri'])
+        else
+          expect(bay['logicalInterconnectGroupUri']).to_not be
+        end
       end
     end
 
