34a5bab53e19aed6094a08dedd9c7a9d09dc5f98
Alex Monteiro <aalexmonteiro@gmail.com>
2017-01-18 15:26:04 +0000

    Implementig the build plans for i3s support

--- lib/oneview-sdk/image-streamer/resource/api300/build_plan.rb
+++ lib/oneview-sdk/image-streamer/resource/api300/build_plan.rb
@@ -17,6 +17,22 @@ module OneviewSDK
       # Build Plan resource implementation for Image Streamer
       class BuildPlan < Resource
         BASE_URI = '/rest/build-plans'.freeze
+
+        # Create a resource object, associate it with a client, and set its properties.
+        # @param [OneviewSDK::ImageStreamer::Client] client The client object for the Image Streamer appliance
+        # @param [Hash] params The options for this resource (key-value pairs)
+        # @param [Integer] api_ver The api version to use when interracting with this resource.
+        def initialize(client, params = {}, api_ver = nil)
+          super
+          # Default values:
+          @data['type'] ||= 'OeBuildPlan'
+        end
+
+        # Build step of the build plan.
+        # @param build_step [Array]  The Build step array of the build plan
+        def set_build_step(build_step = [])
+          @data['buildStep'] = build_step
+        end
       end
     end
   end
--- spec/unit/image-streamer/api300/build_plan_spec.rb
+++ spec/unit/image-streamer/api300/build_plan_spec.rb
@@ -1,9 +1,33 @@
 require 'spec_helper'
 
-RSpec.describe OneviewSDK::ImageStreamer::API300::BuildPlan do
+klass = OneviewSDK::ImageStreamer::API300::BuildPlan
+RSpec.describe klass do
   include_context 'shared context'
 
   describe '#initialize' do
+    it 'sets the defaults correctly' do
+      item = klass.new(@client_i3s_300)
+      expect(item['type']).to eq('OeBuildPlan')
+    end
+  end
 
+  describe '#set_build_step' do
+    it 'creates a build plan with build step' do
+      options = [
+        {
+          serialNumber: '1',
+          parameters: 'anystring',
+          planScriptName: 'planScript1',
+          planScriptUri: 'rest/plan-scrips/fake'
+        }
+      ]
+      item = klass.new(@client_i3s_300)
+      item.set_build_step(options)
+      expect(item['buildStep']).to_not be_empty
+      expect(item['buildStep'].first[:serialNumber]).to eq('1')
+      expect(item['buildStep'].first[:parameters]).to eq('anystring')
+      expect(item['buildStep'].first[:planScriptName]).to eq('planScript1')
+      expect(item['buildStep'].first[:planScriptUri]).to eq('rest/plan-scrips/fake')
+    end
   end
 end
