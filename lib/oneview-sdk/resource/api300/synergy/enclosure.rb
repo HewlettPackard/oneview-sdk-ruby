ace3ee85c1071289c2aaa68af34da2fa99f289ef
Jared Smartt <jared.smartt@hpe.com>
2017-02-08 17:05:24 +0000

    Fixed require issues

--- lib/oneview-sdk/resource/api300/synergy/enclosure.rb
+++ lib/oneview-sdk/resource/api300/synergy/enclosure.rb
@@ -9,7 +9,7 @@
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-require_relative '../../api200/enclosure'
+require_relative '../c7000/enclosure'
 
 module OneviewSDK
   module API300
