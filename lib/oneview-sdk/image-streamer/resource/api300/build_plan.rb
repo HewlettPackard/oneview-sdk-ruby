30fb62d2e85c7b3ca7ce55c2b06cf71f129282e6
Jared Smartt <jared.smartt@hpe.com>
2017-02-09 19:20:30 +0000

    Prepare for 4.0.0 release

--- lib/oneview-sdk/image-streamer/resource/api300/build_plan.rb
+++ lib/oneview-sdk/image-streamer/resource/api300/build_plan.rb
@@ -1,4 +1,4 @@
-# (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+# (C) Copyright 2017 Hewlett Packard Enterprise Development LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # You may not use this file except in compliance with the License.
@@ -15,6 +15,9 @@ module OneviewSDK
   module ImageStreamer
     module API300
       # Build Plan resource implementation for Image Streamer
+      # @note This resource is unimplemented/unfinished at this point, so use at your own risk.
+      #   This resource is subject to change drastically in the near future without a major version
+      #   bump, which may break your code.
       class BuildPlan < Resource
         BASE_URI = '/rest/build-plans'.freeze
       end
