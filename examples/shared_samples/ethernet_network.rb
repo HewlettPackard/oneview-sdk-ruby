d04eb7d8362bb4cff9c5d1b7074d43363f6457e9
Alex Monteiro <aalexmonteiro@gmail.com>
2017-04-03 20:53:43 +0000

    Refactoring examples and adding unit tests

--- examples/shared_samples/ethernet_network.rb
+++ examples/shared_samples/ethernet_network.rb
@@ -1,4 +1,4 @@
-# (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+# (C) Copyright 2017 Hewlett Packard Enterprise Development LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # You may not use this file except in compliance with the License.
@@ -22,18 +22,14 @@ require_relative '../_client' # Gives access to @client
 # - API500 for C7000
 # - API500 for Synergy
 
+# variant represents the model[C7000, Synergy]
+variant = ARGV[0]
+
 # Resource Class used in this sample
-# ethernet_class = OneviewSDK::API200::EthernetNetwork
-ethernet_class = OneviewSDK::API300::C7000::EthernetNetwork
-# ethernet_class = OneviewSDK::API300::Synergy::EthernetNetwork
-# ethernet_class = OneviewSDK::API500::C7000::EthernetNetwork
-# ethernet_class = OneviewSDK::API500::Synergy::EthernetNetwork
+ethernet_class = OneviewSDK.resource_named('FCNetwork', @client.api_version, variant)
 
 # Scope class used in this sample
-scope_class = OneviewSDK::API300::C7000::Scope
-# scope_class = OneviewSDK::API300::Synergy::Scope
-# scope_class = OneviewSDK::API500::C7000::Scope
-# scope_class = OneviewSDK::API500::Synergy::Scope
+scope_class = OneviewSDK.resource_named('Scope', @client.api_version, variant) unless @client.api_version.to_i <= 200
 
 options = {
   vlanId:  '1001',
