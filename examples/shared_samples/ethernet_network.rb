225bfc8389c8fd7ebab8e7e232e51057a1e44400
Alex Monteiro <aalexmonteiro@gmail.com>
2017-04-04 14:04:38 +0000

    Adjusting the use variants in examples

--- examples/shared_samples/ethernet_network.rb
+++ examples/shared_samples/ethernet_network.rb
@@ -29,14 +29,11 @@ require_relative '../_client' # Gives access to @client
 # api_version = 500 & variant = C7000 to OneviewSDK::API500::C7000::EthernetNetwork
 # api_version = 500 & variant = Synergy to OneviewSDK::API500::C7000::EthernetNetwork
 
-# variant represents the model[C7000, Synergy]
-variant = ARGV[0]
-
 # Resource Class used in this sample
-ethernet_class = OneviewSDK.resource_named('FCNetwork', @client.api_version, variant)
+ethernet_class = OneviewSDK.resource_named('FCNetwork', @client.api_version)
 
 # Scope class used in this sample
-scope_class = OneviewSDK.resource_named('Scope', @client.api_version, variant) unless @client.api_version.to_i <= 200
+scope_class = OneviewSDK.resource_named('Scope', @client.api_version) unless @client.api_version.to_i <= 200
 
 options = {
   vlanId:  '1001',
--- examples/shared_samples/fc_network.rb
+++ examples/shared_samples/fc_network.rb
@@ -29,14 +29,11 @@ require_relative '../_client' # Gives access to @client
 # api_version = 500 & variant = C7000 to OneviewSDK::API500::C7000::FCNetwork
 # api_version = 500 & variant = Synergy to OneviewSDK::API500::C7000::FCNetwork
 
-# variant represents the model[C7000, Synergy]
-variant = ARGV[0]
-
 # Resource Class used in this sample
-fc_network_class = OneviewSDK.resource_named('FCNetwork', @client.api_version, variant)
+fc_network_class = OneviewSDK.resource_named('FCNetwork', @client.api_version)
 
 # Scope class used in this sample
-scope_class = OneviewSDK.resource_named('Scope', @client.api_version, variant) unless @client.api_version.to_i <= 200
+scope_class = OneviewSDK.resource_named('Scope', @client.api_version) unless @client.api_version.to_i <= 200
 
 options = {
   name: 'OneViewSDK Test FC Network',
