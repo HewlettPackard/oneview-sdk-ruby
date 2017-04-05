05c663acf38533dc1140b549c4a011a21cb25c23
Alex Monteiro <aalexmonteiro@gmail.com>
2017-04-05 16:24:29 +0000

    Refactoring examples and tests for logical switch group

--- spec/integration/resource/api200/logical_switch_group/delete_spec.rb
+++ spec/integration/resource/api200/logical_switch_group/delete_spec.rb
@@ -2,18 +2,6 @@ require 'spec_helper'
 
 klass = OneviewSDK::LogicalSwitchGroup
 RSpec.describe klass, integration: true, type: DELETE, sequence: rseq(klass) do
-  include_context 'integration context'
-
-  before :all do
-    @item = klass.new($client, name: LOG_SWI_GROUP_NAME)
-    @item.retrieve!
-  end
-
-  describe '#delete' do
-    it 'removes all the Logical Switch groups' do
-      expect { @item.delete }.to_not raise_error
-      item = klass.find_by($client, name: LOG_SWI_GROUP_NAME)
-      expect(item).to be_empty
-    end
-  end
+  let(:current_client) { $client }
+  include_examples 'LogicalSwitchGroupDeleteExample', 'integration context'
 end
--- spec/integration/resource/api500/c7000/logical_switch_group/delete_spec.rb
+++ spec/integration/resource/api500/c7000/logical_switch_group/delete_spec.rb
@@ -2,15 +2,6 @@ require 'spec_helper'
 
 klass = OneviewSDK::API500::C7000::LogicalSwitchGroup
 RSpec.describe klass, integration: true, type: DELETE, sequence: rseq(klass) do
-  include_context 'integration api500 context'
-
-  describe '#delete' do
-    it 'removes all the Logical Switch groups' do
-      item = klass.new($client_500, name: LOG_SWI_GROUP_NAME)
-      item.retrieve!
-      expect { item.delete }.to_not raise_error
-      item = klass.find_by($client_500, name: LOG_SWI_GROUP_NAME)
-      expect(item).to be_empty
-    end
-  end
+  let(:current_client) { $client_500 }
+  include_examples 'LogicalSwitchGroupDeleteExample', 'integration api500 context'
 end
