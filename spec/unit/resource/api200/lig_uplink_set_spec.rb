e63097c295a8ce1715c3b33774c1acf72dcec458
Stepan Vrany <vrany@hpe.com>
2017-05-08 15:32:41 +0200

    QSFP uplink ports support. Unit tests. Changelog.

--- spec/unit/resource/api200/lig_uplink_set_spec.rb
+++ spec/unit/resource/api200/lig_uplink_set_spec.rb
@@ -15,29 +15,61 @@ RSpec.describe OneviewSDK::LIGUplinkSet do
 
   describe 'adds' do
     def_options = { networkType: 'Ethernet', ethernetNetworkType: 'NotApplicable' }
-    it 'Add empty network resource' do
+    it 'empty network resource' do
       upset = OneviewSDK::LIGUplinkSet.new(@client_200, def_options)
       net = OneviewSDK::EthernetNetwork.new(@client_200, {})
       expect { upset.add_network(net) }.to raise_error(OneviewSDK::IncompleteResource, /Must set/)
     end
-    it 'Add retrieved network resource' do
+    it 'retrieved network resource' do
       options = { networkType: 'Ethernet', ethernetNetworkType: 'NotApplicable' }
       upset = OneviewSDK::LIGUplinkSet.new(@client_200, options)
       net = OneviewSDK::EthernetNetwork.new(@client_200, uri: '/rest/ethernet-networks/65546B-A55F20-663390-CA96F5')
       expect { upset.add_network(net) }.not_to raise_error
     end
-    it 'Add X7 port' do
-      upset = OneviewSDK::LIGUplinkSet.new(@client_200, def_options)
-      expect { upset.add_uplink(1, 'X7') }.not_to raise_error
+    context 'statically obtained relative value of' do
+      it 'port X7' do
+        upset = OneviewSDK::LIGUplinkSet.new(@client_200, def_options)
+        expect { upset.add_uplink(1, 'X7') }.not_to raise_error
+      end
+      it 'port D5' do
+        upset = OneviewSDK::LIGUplinkSet.new(@client_200, def_options)
+        expect { upset.add_uplink(1, 'D5') }.not_to raise_error
+      end
+      it 'port 67' do
+        upset = OneviewSDK::LIGUplinkSet.new(@client_200, def_options)
+        expect { upset.add_uplink(1, '67') }.not_to raise_error
+      end
+      it 'not supported port' do
+        upset = OneviewSDK::LIGUplinkSet.new(@client_200, def_options)
+        expect { upset.add_uplink(1, 'V5') }.to raise_error(OneviewSDK::InvalidResource, /Port not supported/)
+      end
     end
-    it 'Add D5 port' do
-      upset = OneviewSDK::LIGUplinkSet.new(@client_200, def_options)
-      expect { upset.add_uplink(1, 'D5') }.not_to raise_error
-    end
-    it 'Add not supported port' do
-      upset = OneviewSDK::LIGUplinkSet.new(@client_200, def_options)
-      expect { upset.add_uplink(1, 'V5') }.to raise_error(OneviewSDK::InvalidResource, /Port not supported/)
+    context 'dynamically obtained relative value of' do
+      before :each do
+        @type = 'HP VC FlexFabric-20/40 F8 Module'
+        resp = { 'portInfos' => [
+          { 'portName' => 'Q8:1', 'portNumber' => 97 },
+          { 'portName' => 'Q1.1', 'portNumber' => 17 },
+          { 'portName' => 'Q1', 'portNumber' => 61 }
+          ] }
+        allow(OneviewSDK::Interconnect).to receive(:get_type).with(@client_200, @type).and_return(resp)
+      end
+      it 'port Q8:1' do
+        upset = OneviewSDK::LIGUplinkSet.new(@client_200, def_options)
+        expect { upset.add_uplink(1, 'Q8:1', @type) }.not_to raise_error
+      end
+      it 'port Q1' do
+        upset = OneviewSDK::LIGUplinkSet.new(@client_200, def_options)
+        expect { upset.add_uplink(1, 'Q1', @type) }.not_to raise_error
+      end
+      it 'port Q1.1' do
+        upset = OneviewSDK::LIGUplinkSet.new(@client_200, def_options)
+        expect { upset.add_uplink(1, 'Q1.1', @type) }.not_to raise_error
+      end
+      it 'not supported port' do
+        upset = OneviewSDK::LIGUplinkSet.new(@client_200, def_options)
+        expect { upset.add_uplink(1, 'Q8:1', @type) }.to raise_error(OneviewSDK::NotFound)
+      end
     end
   end
-
 end
