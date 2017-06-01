3b0107e0de726e4e3c3f1b692d2e1cde6b497228
Ricardo Galeno <ricardogaleno.rg@gmail.com>
2017-06-01 17:08:58 -0300

    changes to StoragePool API500 based on review comments

--- spec/integration/resource/api300/synergy/storage_pool/delete_spec.rb
+++ spec/integration/resource/api300/synergy/storage_pool/delete_spec.rb
@@ -4,5 +4,12 @@ klass = OneviewSDK::API300::Synergy::StoragePool
 RSpec.describe klass, integration: true, type: DELETE, sequence: rseq(klass) do
   include_examples 'StoragePoolDeleteExample', 'integration api300 context' do
     let(:current_client) { $client_300_synergy }
+    let(:storage_system_options) do
+      {
+        credentials: {
+          ip_hostname: $secrets_synergy['storage_system1_ip']
+        }
+      }
+    end
   end
 end
