b9aaded9addf63593268e6dfcb74ccce10eb9540
Alex Monteiro <aalexmonteiro@gmail.com>
2017-06-12 18:19:52 +0000

    Implementing the user for api 500

--- spec/integration/resource/api300/synergy/user/create_spec.rb
+++ spec/integration/resource/api300/synergy/user/create_spec.rb
@@ -2,25 +2,6 @@ require 'spec_helper'
 
 klass = OneviewSDK::API300::Synergy::User
 RSpec.describe klass, integration: true, type: CREATE, sequence: seq(klass) do
-  include_context 'integration context'
-
-  describe '#create' do
-    it 'can create resources' do
-      options = {
-        userName: USER_NAME,
-        password: 'secret123',
-        emailAddress: 'test_user@hpe.com',
-        fullName: 'Test User',
-        roles: ['Read only']
-      }
-
-      item = klass.new($client, options)
-
-      expect { item.create }.to_not raise_error
-      expect(item[:userName]).to eq(USER_NAME)
-      expect(item[:emailAddress]).to eq('test_user@hpe.com')
-      expect(item[:fullName]).to eq('Test User')
-      expect(item[:roles]).to eq(['Read only'])
-    end
-  end
+  let(:current_client) { $client_300_synergy }
+  include_examples 'UserCreateExample', 'integration api300 context'
 end
