23039cd5cceb3e5605c2c0cf43f4401b299fcd6f
Alex Monteiro <aalexmonteiro@gmail.com>
2017-06-19 15:57:17 +0000

    Implementing the firmware bundle api 500

--- spec/integration/resource/api500/c7000/firmware_bundle/create_spec.rb
+++ spec/integration/resource/api500/c7000/firmware_bundle/create_spec.rb
@@ -11,11 +11,11 @@
 
 require 'spec_helper'
 
-klass = OneviewSDK::API300::C7000::FirmwareBundle
+klass = OneviewSDK::API500::C7000::FirmwareBundle
 RSpec.describe klass, integration: true, type: CREATE, sequence: seq(klass) do
-  let(:current_client) { $client_300 }
+  let(:current_client) { $client_500 }
   let(:bundle_path) { $secrets['bundle_path'] }
   let(:spp_path) { $secrets['spp_path'] }
 
-  include_examples 'FirmwareBundleCreateExample', 'integration api300 context'
+  include_examples 'FirmwareBundleCreateExample', 'integration api500 context'
 end
