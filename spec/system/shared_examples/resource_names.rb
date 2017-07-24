6d66e1158c8720b627c65de208ad39ad85834585
Ricardo Galeno <ricardogaleno.rg@gmail.com>
2017-07-24 18:29:45 -0300

    Adjusts system tests to run using real hardware

--- spec/system/shared_examples/resource_names.rb
+++ spec/system/shared_examples/resource_names.rb
@@ -9,14 +9,16 @@ class ResourceNames
     attr_accessor :storage_pool
     attr_accessor :volume
     attr_accessor :volume_template
+    attr_accessor :scope
   end
 
   self.ethernet_network = ['EthernetNetwork_01']
-  self.bulk_ethernet_network = ['BulkEthernetNetwork']
+  self.bulk_ethernet_network = [{ namePrefix: 'BulkEthernetNetwork', vlanIdRange: '2-6' }]
   self.fc_network = ['FCNetwork_01']
   self.fcoe_network = ['FCoENetwork_01']
-  self.storage_system = %w(ThreePAR-1 Cluster-1)
-  self.storage_pool = %w(CPG-SSD-AO Cluster-1)
+  self.storage_system = %w(eco-3par ThreePAR-1 Cluster-1)
+  self.storage_pool = %w(ESX_Shared_Storage cpg-growth-limit-1TiB CPG-SSD-AO Cluster-1)
   self.volume = %w(Volume_01 VolumeVirtual_01)
   self.volume_template = %w(VolumeTemplate_01 VolumeVirtualTemplate_01)
+  self.scope = %w(Scope01)
 end
