f50f29f83a168ad5520eca9144d679bf6f696524
Ricardo Galeno <ricardogaleno.rg@gmail.com>
2017-08-02 14:59:25 -0300

    Fixed integration test setup of Logical Interconnect Group for apis 300 and 500

--- spec/integration/resource/api300/c7000/logical_interconnect_group/update_spec.rb
+++ spec/integration/resource/api300/c7000/logical_interconnect_group/update_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe klass, integration: true, type: UPDATE do
   let(:lig_uplink_set_class) { OneviewSDK::API300::C7000::LIGUplinkSet }
   let(:ethernet_network_class) { OneviewSDK::API300::C7000::EthernetNetwork }
 
-  include_examples 'LIGC7000UpdateExample', 'integration api300 context'
+  it_behaves_like 'LIGC7000UpdateExample', 'integration api300 context'
 
-  include_examples 'ScopeHelperMethodsExample', OneviewSDK::API300::C7000::Scope do
+  it_behaves_like 'ScopeHelperMethodsExample', OneviewSDK::API300::C7000::Scope do
     let(:item) { described_class.get_all(current_client).first }
   end
 end
