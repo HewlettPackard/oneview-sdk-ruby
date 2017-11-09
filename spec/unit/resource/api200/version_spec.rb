5726b8d4bf9d9c974a7e3e7783867f4f19255210
Sood <priyanka.sood@hpe.com>
2017-11-09 12:55:54 +0530

    Fixed the issue to call rest_get by calling expect instead of allow

--- spec/unit/resource/api200/version_spec.rb
+++ spec/unit/resource/api200/version_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe OneviewSDK::Version do
   include_context 'shared context'
   describe '#get_version' do
     it 'calls the uri' do
-      allow(@client_200).to receive(:rest_get).with(described_class::BASE_URI, {})
+      expect(@client_200).to receive(:rest_get).with(described_class::BASE_URI).and_return(FakeResponse.new(members: []))
+      described_class.get_version(@client_200)
     end
   end
 end
