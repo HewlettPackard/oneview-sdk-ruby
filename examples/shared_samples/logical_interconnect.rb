9f76cbed4b353215f4455dd6e524d87f9a2a2dd3
Madhav <madhavb@hpe.com>
2018-03-14 08:30:45 -0400

    Updating example file documentation

--- examples/shared_samples/logical_interconnect.rb
+++ examples/shared_samples/logical_interconnect.rb
@@ -14,7 +14,7 @@ require_relative '../_client' # Gives access to @client
 # Example: Explores functionalities of Logical Interconnects
 #
 # Supported APIs:
-# - 200, 300, 500
+# - 200, 300, 500, 600
 
 # Resources that can be created according to parameters:
 # api_version = 200 & variant = any to OneviewSDK::API200::LogicalInterconnect
@@ -22,6 +22,8 @@ require_relative '../_client' # Gives access to @client
 # api_version = 300 & variant = Synergy to OneviewSDK::API300::Synergy::LogicalInterconnect
 # api_version = 500 & variant = C7000 to OneviewSDK::API500::C7000::LogicalInterconnect
 # api_version = 500 & variant = Synergy to OneviewSDK::API500::Synergy::LogicalInterconnect
+# api_version = 600 & variant = C7000 to OneviewSDK::API600::C7000::LogicalInterconnect
+# api_version = 600 & variant = Synergy to OneviewSDK::API600::Synergy::LogicalInterconnect
 
 # Resource Class used in this sample
 logical_interconnect_class = OneviewSDK.resource_named('LogicalInterconnect', @client.api_version)
