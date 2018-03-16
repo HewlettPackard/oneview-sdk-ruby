0f14809d9adf273108851c00ea9fb1174d793245
soodpr <priyanka.sood@hpe.com>
2018-03-16 13:45:39 -0400

    Adding scopeeUris  query param

--- examples/shared_samples/sas_logical_interconnect_group.rb
+++ examples/shared_samples/sas_logical_interconnect_group.rb
@@ -15,20 +15,23 @@ require_relative '../_client' # Gives access to @client
 # NOTE: This will create a sas logical interconnect group named 'ONEVIEW_SDK_SAMPLE_SAS_LIG', update it and then delete it.
 #
 # Supported APIs:
-# - 300, 500,  600
+# - 300, 500
 
 # Resources that can be created according to parameters:
 # api_version = 300 & variant = Synergy to OneviewSDK::API300::Synergy::SASLogicalInterconnectGroup
 # api_version = 500 & variant = Synergy to OneviewSDK::API500::Synergy::SASLogicalInterconnectGroup
-# api_version = 600 & variant = Synergy to OneviewSDK::API600::Synergy::SASLogicalInterconnectGroup
 
 # Resource Class used in this sample
 sas_int_group_class = OneviewSDK.resource_named('SASLogicalInterconnectGroup', @client.api_version)
+scope_class = OneviewSDK.resource_named('Scope', @client.api_version)
 
-type = 'sas-logical-interconnect-group'
+scope = scope_class.get_all(@client).first
+
+type = 'SAS Logical Interconnect Group'
 
 options = {
-  name: 'ONEVIEW_SDK_SAMPLE_SAS_LIG'
+  name: 'ONEVIEW_SDK_SAMPLE_SAS_LIG',
+  initialScopeUris: ['/rest/scopes/ed1e27d2-1940-481b-bf05-a94418ab17dc']
 }
 
 item = sas_int_group_class.new(@client, options)
@@ -40,6 +43,16 @@ item.add_interconnect(4, @sas_interconnect_type)
 puts "\nCreating a #{type} with name = #{item[:name]}."
 item.create!
 puts "\n#{type} #{item[:name]} created!"
+
+# Gets a  SAS LogicalInterconnectGroup by scopeUris
+query = {
+  scopeUris: scope['uri']
+}
+if @client.api_version >= 600
+  puts "\nGets a SAS LogicalInterconnectGroup with scope '#{query[:scopeUris]}'"
+  item2 = sas_int_group_class.get_all_with_query(@client, query)
+  puts "Found SAS LogicalInterconnectGroup'#{item2}'."
+end
 
 options2 = {
   name: 'UPDATED_ONEVIEW_SDK_SAMPLE_SAS_LIG '
