af4e119d8e6ad67a89b1dbebc6959928cd4ee9ca
Sijeesh Kattumunda <mailtosije@gmail.com>
2018-03-19 13:05:53 -0600

    Completed coding with test cases.

--- spec/integration/resource/api600/c7000/server_profile_template/create_spec.rb
+++ spec/integration/resource/api600/c7000/server_profile_template/create_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-klass = OneviewSDK::API500::C7000::ServerProfileTemplate
+klass = OneviewSDK::API600::C7000::ServerProfileTemplate
 RSpec.describe klass, integration: true, type: CREATE, sequence: seq(klass) do
-  let(:current_client) { $client_500 }
+  let(:current_client) { $client_600 }
   let(:storage_system_ip) { $secrets['storage_system1_ip'] }
   let(:server_hardware_type_name) { SERVER_HARDWARE_TYPE_NAME }
-  include_examples 'ServerProfileTemplateCreateExample API500', 'integration api500 context'
+  include_examples 'ServerProfileTemplateCreateExample', 'integration api600 context'
 end
--- spec/integration/resource/api600/c7000/server_profile_template/update_spec.rb
+++ spec/integration/resource/api600/c7000/server_profile_template/update_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe OneviewSDK::API500::C7000::ServerProfileTemplate, integration: true, type: UPDATE do
-  let(:current_client) { $client_500 }
-  include_examples 'ServerProfileTemplateUpdateExample', 'integration api500 context'
+RSpec.describe OneviewSDK::API600::C7000::ServerProfileTemplate, integration: true, type: UPDATE do
+  let(:current_client) { $client_600 }
+  include_examples 'ServerProfileTemplateUpdateExample', 'integration api600 context'
 end
