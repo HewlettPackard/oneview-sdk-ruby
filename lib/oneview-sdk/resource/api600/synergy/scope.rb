2d96ebea43d2b1742889521c58b11b96e48765fe
Jyothis Gireesan Mini <jyothis.gireesan-mini@hpe.com>
2018-03-20 16:00:54 +0530

    Added initialize for scope

--- lib/oneview-sdk/resource/api600/synergy/scope.rb
+++ lib/oneview-sdk/resource/api600/synergy/scope.rb
@@ -16,6 +16,16 @@ module OneviewSDK
     module Synergy
       # Scope resource implementation for API600 Synergy
       class Scope < OneviewSDK::API600::C7000::Scope
+        # Create a resource object, associate it with a client, and set its properties.
+        # @param [OneviewSDK::Client] client The client object for the OneView appliance
+        # @param [Hash] params The options for this resource (key-value pairs)
+        # @param [Integer] api_ver The api version to use when interracting with this resource.
+        def initialize(client, params = {}, api_ver = nil)
+          @data ||= {}
+          # Default values:
+          @data['type'] ||= 'ScopeV3'
+          super
+        end
       end
     end
   end
