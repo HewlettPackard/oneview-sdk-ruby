741103f0b31231557c5c7a8bc9a343c6510c9df4
soodpr <priyanka.sood@hpe.com>
2018-03-21 18:18:51 +0530

    Adding systems tests for api600

---
+++ spec/system/light_profile/api600/c7000/clean_up_spec.rb
@@ -0,0 +1,34 @@
+# System test script
+# Light Profie
+
+require 'spec_helper'
+
+RSpec.describe 'Clean up fluid resource pool API600 C7000', system: true, sequence: 1, api_version: 600, model: 'C7000' do
+  let(:current_client) { $client_600 }
+  let(:storage_system_options) do
+    {
+      credentials: {
+        username: $secrets['storage_system1_user'],
+        password: $secrets['storage_system1_password']
+      },
+      hostname: $secrets['storage_system1_ip'],
+      family: 'StoreServ',
+      deviceSpecificAttributes: {
+        managedDomain: 'TestDomain'
+      }
+    }
+  end
+
+  let(:storage_virtual_system_options) do
+    {
+      credentials: {
+        username: $secrets['store_virtual_user'],
+        password: $secrets['store_virtual_password']
+      },
+      hostname: $secrets['store_virtual_ip'],
+      family: 'StoreVirtual'
+    }
+  end
+  include_examples 'Clean up SystemTestExample', 'system API600 context'
+  include_examples 'Clean up SystemTestExample C7000', 'system API600 context'
+end
