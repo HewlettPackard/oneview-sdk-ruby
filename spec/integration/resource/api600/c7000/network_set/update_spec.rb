619743e6590fcbf8bcf69ee6d93a61df1debbb89
Jyothis Gireesan Mini <jyothis.gireesan-mini@hpe.com>
2018-03-22 00:36:36 +0530

    Network sets for V600

--- spec/integration/resource/api600/c7000/network_set/update_spec.rb
+++ spec/integration/resource/api600/c7000/network_set/update_spec.rb
@@ -1,12 +1,9 @@
 require 'spec_helper'
 
-RSpec.describe OneviewSDK::API500::C7000::NetworkSet, integration: true, type: UPDATE, sequence: 2 do
-  let(:current_client) { $client_500 }
-  let(:ethernet_class) { OneviewSDK::API500::C7000::EthernetNetwork }
+RSpec.describe OneviewSDK::API600::C7000::NetworkSet, integration: true, type: UPDATE, sequence: 2 do
+  let(:current_client) { $client_600 }
+  let(:ethernet_class) { OneviewSDK::API600::C7000::EthernetNetwork }
 
-  include_examples 'NetworkSetUpdateExample', 'integration api500 context'
-
-  include_examples 'ScopeHelperMethodsExample', OneviewSDK::API500::C7000::Scope do
-    let(:item) { described_class.get_all(current_client).first }
+  include_examples 'NetworkSetUpdateExample', 'integration api600 context'
   end
 end
--- spec/unit/resource/api600/synergy/network_set_spec.rb
+++ spec/unit/resource/api600/synergy/network_set_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-RSpec.describe OneviewSDK::API500::Synergy::NetworkSet do
+RSpec.describe OneviewSDK::API600::Synergy::NetworkSet do
   include_context 'shared context'
 
-  it 'inherits from OneviewSDK::API300::Synergy::NetworkSet' do
-    expect(described_class).to be < OneviewSDK::API300::Synergy::NetworkSet
+  it 'inherits from OneviewSDK::API500::Synergy::NetworkSet' do
+    expect(described_class).to be < OneviewSDK::API500::Synergy::NetworkSet
   end
 end
