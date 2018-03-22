a5aa6dd14a87cce1ba3841dcd1377b7918fe3216
Sijeesh Kattumunda <mailtosije@gmail.com>
2018-03-22 04:09:10 -0600

    Updated server profile template integration test.

--- spec/integration/resource/api800/c7000/server_profile_template/delete_spec.rb
+++ spec/integration/resource/api800/c7000/server_profile_template/delete_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-klass = OneviewSDK::API500::C7000::ServerProfileTemplate
+klass = OneviewSDK::API800::C7000::ServerProfileTemplate
 RSpec.describe klass, integration: true, type: DELETE, sequence: rseq(klass) do
-  let(:current_client) { $client_500 }
-  include_examples 'ServerProfileTemplateDeleteExample', 'integration api500 context'
+  let(:current_client) { $client_800 }
+  include_examples 'ServerProfileTemplateDeleteExample', 'integration api800 context'
 end
--- spec/integration/resource/api800/c7000/server_profile_template/update_spec.rb
+++ spec/integration/resource/api800/c7000/server_profile_template/update_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe OneviewSDK::API500::C7000::ServerProfileTemplate, integration: true, type: UPDATE do
-  let(:current_client) { $client_500 }
-  include_examples 'ServerProfileTemplateUpdateExample', 'integration api500 context'
+RSpec.describe OneviewSDK::API800::C7000::ServerProfileTemplate, integration: true, type: UPDATE do
+  let(:current_client) { $client_800 }
+  include_examples 'ServerProfileTemplateUpdateExample', 'integration api800 context'
 end
