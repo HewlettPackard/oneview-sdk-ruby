792dfcbba0529f0eefabd4aef5981d0a9071e2dd
Sijeesh Kattumunda <mailtosije@gmail.com>
2018-03-26 01:43:48 -0600

    Updated server profile resource files and test files.

--- spec/integration/resource/api600/c7000/server_profile/update_spec.rb
+++ spec/integration/resource/api600/c7000/server_profile/update_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-klass = OneviewSDK::API500::C7000::ServerProfile
+klass = OneviewSDK::API600::C7000::ServerProfile
 RSpec.describe klass, integration: true, type: UPDATE do
-  let(:current_client) { $client_500 }
+  let(:current_client) { $client_600 }
   let(:server_hardware_type_name) { SERVER_HARDWARE_TYPE_NAME }
   let(:storage_system_ip) { $secrets['storage_system1_ip'] }
-  include_examples 'ServerProfileUpdateExample', 'integration api500 context'
+  include_examples 'ServerProfileUpdateExample', 'integration api600 context'
 end
