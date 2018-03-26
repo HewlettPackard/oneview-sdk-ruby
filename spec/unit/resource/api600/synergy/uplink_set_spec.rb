8bda17803793f226289845601b4eb66c6a9df4ba
AnushaY <anuanusha471@gmail.com>
2018-03-26 05:24:36 -0700

    Minor change

--- spec/unit/resource/api600/synergy/uplink_set_spec.rb
+++ spec/unit/resource/api600/synergy/uplink_set_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe OneviewSDK::API600::Synergy::UplinkSet do
   include_context 'shared context'
 
-  it 'inherits from OneviewSDK::API600::Synergy::UplinkSet' do
+  it 'inherits from OneviewSDK::API500::Synergy::UplinkSet' do
     expect(described_class).to be < OneviewSDK::API500::Synergy::UplinkSet
   end
 end
