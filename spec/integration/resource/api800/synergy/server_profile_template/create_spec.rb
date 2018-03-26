e0c61dd71413b3a2f5d8cef119a93772b592a321
Sijeesh Kattumunda <mailtosije@gmail.com>
2018-03-26 05:20:35 -0600

    Updated integration test files for server profile templates.

--- spec/integration/resource/api800/synergy/server_profile_template/create_spec.rb
+++ spec/integration/resource/api800/synergy/server_profile_template/create_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-klass = OneviewSDK::API500::Synergy::ServerProfileTemplate
+klass = OneviewSDK::API800::Synergy::ServerProfileTemplate
 RSpec.describe klass, integration: true, type: CREATE, sequence: seq(klass) do
-  let(:current_client) { $client_500_synergy }
+  let(:current_client) { $client_800_synergy }
   let(:storage_system_ip) { $secrets_synergy['storage_system1_ip'] }
   let(:server_hardware_type_name) { SERVER_HARDWARE_TYPE2_NAME }
-  include_examples 'ServerProfileTemplateCreateExample API500', 'integration api500 context'
+  include_examples 'ServerProfileTemplateCreateExample API500', 'integration api800 context'
 end
