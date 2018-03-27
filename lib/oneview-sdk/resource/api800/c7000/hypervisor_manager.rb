8e8c06b4576d2b30f20bacb959130dde171315a0
soodpr <priyanka.sood@hpe.com>
2018-03-27 21:20:38 +0530

    Removing resource.rb file from both variants for API800

--- lib/oneview-sdk/resource/api800/c7000/hypervisor_manager.rb
+++ lib/oneview-sdk/resource/api800/c7000/hypervisor_manager.rb
@@ -8,13 +8,13 @@
 # under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-require_relative 'resource'
+require_relative '../../../resource'
 
 module OneviewSDK
   module API800
     module C7000
       # Hypervisor Manager resource implementation
-      class HypervisorManager < Resource
+      class HypervisorManager < OneviewSDK::Resource
         BASE_URI = '/rest/hypervisor-managers'.freeze
 
         # Create a resource object, associate it with a client, and set its properties.
