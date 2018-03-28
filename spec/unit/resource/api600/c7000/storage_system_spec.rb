e714af79d887757eee9c6e5db6b1bee1ee5581e9
soodpr <priyanka.sood@hpe.com>
2018-03-28 13:04:35 +0530

    Removing extra unit test for c7000 storage systems

--- spec/unit/resource/api600/c7000/storage_system_spec.rb
+++ spec/unit/resource/api600/c7000/storage_system_spec.rb
@@ -6,10 +6,6 @@ RSpec.describe OneviewSDK::API600::C7000::StorageSystem do
   let(:client) { @client_600 }
   let(:empty_item) { described_class.new(client) }
 
-  it 'the UNIQUE_IDENTIFIERS shoud have name, uri and hostname' do
-    expect(described_class::UNIQUE_IDENTIFIERS).to eq(%w[name uri hostname])
-  end
-
   it 'inherits from OneviewSDK::API500::C7000::StorageSystem' do
     expect(described_class).to be < OneviewSDK::API500::C7000::StorageSystem
   end
