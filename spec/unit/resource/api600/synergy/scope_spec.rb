982699304fa91d82c39ac2788ccd669d0141861b
Sijeesh Kattumunda <mailtosije@gmail.com>
2018-03-29 13:36:18 -0600

    Added test class for synergy

--- spec/unit/resource/api600/synergy/scope_spec.rb
+++ spec/unit/resource/api600/synergy/scope_spec.rb
@@ -11,10 +11,10 @@
 
 require 'spec_helper'
 
-RSpec.describe OneviewSDK::API500::Synergy::Scope do
+RSpec.describe OneviewSDK::API600::Synergy::Scope do
   include_context 'shared context'
 
-  it 'inherits from OneviewSDK::API500::Synergy::Scope' do
-    expect(described_class).to be < OneviewSDK::API500::C7000::Scope
+  it 'inherits from OneviewSDK::API600::Synergy::Scope' do
+    expect(described_class).to be < OneviewSDK::API600::C7000::Scope
   end
 end
