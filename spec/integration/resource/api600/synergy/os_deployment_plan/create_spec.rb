304dd53dee244cf4fbbbac06f87a1209c0f8f355
Madhav <madhavb@hpe.com>
2018-04-12 06:16:44 -0400

    Api600 support for OS Deployment Plans

--- spec/integration/resource/api600/synergy/os_deployment_plan/create_spec.rb
+++ spec/integration/resource/api600/synergy/os_deployment_plan/create_spec.rb
@@ -11,8 +11,8 @@
 
 require 'spec_helper'
 
-klass = OneviewSDK::API500::Synergy::OSDeploymentPlan
+klass = OneviewSDK::API600::Synergy::OSDeploymentPlan
 RSpec.describe klass, integration: true, type: CREATE, sequence: seq(klass) do
-  let(:current_client) { $client_500_synergy }
-  include_examples 'OSDeploymentPlanCreateExample', 'integration api500 context'
+  let(:current_client) { $client_600_synergy }
+  include_examples 'OSDeploymentPlanCreateExample', 'integration api600 context'
 end
--- spec/unit/resource/api600/synergy/os_deployment_plan_spec.rb
+++ spec/unit/resource/api600/synergy/os_deployment_plan_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe OneviewSDK::API500::Synergy::OSDeploymentPlan do
+RSpec.describe OneviewSDK::API600::Synergy::OSDeploymentPlan do
   include_context 'shared context'
 
   it 'inherits from OneviewSDK::API300::Synergy::OSDeploymentPlan' do
