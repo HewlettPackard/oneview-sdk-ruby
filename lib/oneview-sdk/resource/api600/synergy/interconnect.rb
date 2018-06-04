8a12c3a5340e402272ad9af04508565196fcaa91
Madhav <madhavb@hpe.com>
2018-06-04 01:52:39 -0400

    adding missing method to api600 lib

--- lib/oneview-sdk/resource/api600/synergy/interconnect.rb
+++ lib/oneview-sdk/resource/api600/synergy/interconnect.rb
@@ -17,6 +17,21 @@ module OneviewSDK
     module Synergy
       # Interconnect resource implementation on API600 Synergy
       class Interconnect < OneviewSDK::API600::C7000::Interconnect
+
+         # Retrieves the interconnect link topologies
+        # @param [OneviewSDK::Client] client The client object for the OneView appliance
+        # @return [Array] All the Interconnect Link Topologies
+        def self.get_link_topologies(client)
+          OneviewSDK::API300::Synergy::Interconnect.get_link_topologies(client)
+        end
+
+        # Retrieves the interconnect link topology with the name
+        # @param [OneviewSDK::Client] client The client object for the OneView appliance
+        # @param [String] name Switch type name
+        # @return [Array] Switch type
+        def self.get_link_topology(client, name)
+          OneviewSDK::API300::Synergy::Interconnect.get_link_topology(client, name)
+        end
       end
     end
   end
