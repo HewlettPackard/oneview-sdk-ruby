503c6303bd2a69769bbde3ddf5ec6f3c95c8eacb
Enrico Nemitz <enrico.nemitz@telekom.de>
2019-04-04 09:03:07 +0200

    resource to API 500 Synergy
    The resource was set to C7000, but most of the Logical Interconnect Group functionality for the Synergy are build in '../../api300/synergy'.
    The recource for api500 is also set to api300.

--- spec/unit/resource/api600/synergy/logical_interconnect_group.rb
+++ spec/unit/resource/api600/synergy/logical_interconnect_group.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe OneviewSDK::API600::Synergy::LogicalInterconnectGroup do
   include_context 'shared context'
 
-  it 'inherits from OneviewSDK::API600::C7000::LogicalInterconnectGroup' do
-    expect(described_class).to be < OneviewSDK::API600::C7000::LogicalInterconnectGroup
+  it 'inherits from OneviewSDK::API500::Synergy::LogicalInterconnectGroup' do
+    expect(described_class).to be < OneviewSDK::API500::Synergy::LogicalInterconnectGroup
   end
 end
