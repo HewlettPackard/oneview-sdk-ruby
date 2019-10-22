69b6c063b5d29339527403763aa75e2c8a3eded9
Priyanka Sood <priyanka.sood@hpe.com>
2019-10-22 14:37:00 +0530

    fixing build issues by adding proper indentation

--- lib/oneview-sdk/resource/api800/c7000/logical_interconnect.rb
+++ lib/oneview-sdk/resource/api800/c7000/logical_interconnect.rb
@@ -21,11 +21,11 @@ module OneviewSDK
         # @param [Hash] params The options for this resource (key-value pairs)
         # @param [Integer] api_ver The api version to use when interracting with this resource.
         def initialize(client, params = {}, api_ver = nil)
-         @data ||= {}
-         # Default values:
-         @data['type'] ||= 'logical-interconnectV5'
-         super
-       end
+          @data ||= {}
+          # Default values:
+          @data['type'] ||= 'logical-interconnectV5'
+          super
+        end
       end
     end
   end
