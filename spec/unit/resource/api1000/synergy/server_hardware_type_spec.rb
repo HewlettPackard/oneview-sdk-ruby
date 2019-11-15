e1a577a6b10f2b603b6ce9cdc2b1f7f02ebdf6fb
Priyanka Sood <priyanka.sood@hpe.com>
2019-11-15 12:45:12 +0530

    Fixinf unit test cases

--- spec/unit/resource/api1000/synergy/server_hardware_type_spec.rb
+++ spec/unit/resource/api1000/synergy/server_hardware_type_spec.rb
@@ -3,14 +3,7 @@ require 'spec_helper'
 RSpec.describe OneviewSDK::API1000::Synergy::ServerHardwareType do
   include_context 'shared context'
 
-  it 'inherits from OneviewSDK::API800::Synergy::ServerHardwareType' do
-    expect(described_class).to be < OneviewSDK::API800::Synergy::ServerHardwareType
-  end
-
-  describe '#initialize' do
-    it 'should be initialize the instance with default values' do
-      item = described_class.new(@client_1000)
-      expect(item['type']).to eq('server-hardware-type-10')
-    end
-  end
+  it 'inherits from OneviewSDK::API1000::C7000::ServerHardwareType' do
+    expect(described_class).to be < OneviewSDK::API1000::C7000::ServerHardwareType
+  end  
 end
