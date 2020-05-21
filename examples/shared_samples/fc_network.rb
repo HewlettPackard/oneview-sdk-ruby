68dd40f56799916474a75a2a11441948f0c61a9c
yuvirani <yuvaranisrec@gmail.com>
2020-05-21 21:11:23 +0530

    Update API versions for all network resources

--- examples/shared_samples/fc_network.rb
+++ examples/shared_samples/fc_network.rb
@@ -23,7 +23,15 @@ require_relative '../_client' # Gives access to @client
 # - API500 for Synergy
 # - API600 for C7000
 # - API600 for Synergy
-
+# - API800 for C7000
+# - API800 for Synergy
+# - API1000 for C7000
+# - API1000 for Synergy
+# - API1200 for C7000
+# - API1200 for Synergy
+# - API1600 for C7000
+# - API1600 for Synergy
+#
 # Resources that can be created according to parameters:
 # api_version = 200 & variant = any to OneviewSDK::API200::FCNetwork
 # api_version = 300 & variant = C7000 to OneviewSDK::API300::C7000::FCNetwork
@@ -32,6 +40,15 @@ require_relative '../_client' # Gives access to @client
 # api_version = 500 & variant = Synergy to OneviewSDK::API500::C7000::FCNetwork
 # api_version = 600 & variant = C7000 to OneviewSDK::API600::C7000::FCNetwork
 # api_version = 600 & variant = Synergy to OneviewSDK::API600::C7000::FCNetwork
+# api_version = 800 & variant = C7000 to OneviewSDK::API800::C7000::FCNetwork
+# api_version = 800 & variant = Synergy to OneviewSDK::API800::C7000::FCNetwork
+# api_version = 1000 & variant = C7000 to OneviewSDK::API1000::C7000::FCNetwork
+# api_version = 1000 & variant = Synergy to OneviewSDK::API1000::C7000::FCNetwork
+# api_version = 1200 & variant = C7000 to OneviewSDK::API1200::C7000::FCNetwork
+# api_version = 1200 & variant = Synergy to OneviewSDK::API1200::C7000::FCNetwork
+# api_version = 1600 & variant = C7000 to OneviewSDK::API1600::C7000::FCNetwork
+# api_version = 1600 & variant = Synergy to OneviewSDK::API1600::C7000::FCNetwork
+
 
 # Resource Class used in this sample
 fc_network_class = OneviewSDK.resource_named('FCNetwork', @client.api_version)
