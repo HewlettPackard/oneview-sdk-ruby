f14271e5de8ec4008ea2e261c92cbb02072f2072
Nabhajit Ray <nabhajit.ray@hpe.com>
2020-05-21 00:24:45 -0400

    API1600  support for Storage Pool

---
+++ spec/unit/resource/api1600/c7000/storage_pool_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+RSpec.describe OneviewSDK::API1600::C7000::StoragePool do
+  include_context 'shared context'
+
+  it 'inherits from OneviewSDK::API1200::C7000::VolumeAttachment' do
+    expect(described_class).to be < OneviewSDK::API1200::C7000::StoragePool
+  end
+end
