48df647858b85a2474292bc5caa8e8e83a92df25
Nabhajit Ray <nabhajit.ray@hpe.com>
2020-05-26 03:06:04 -0400

    Fixed API1600 spec file

--- spec/unit/resource/api1600/synergy/volume_attachment_spec.rb
+++ spec/unit/resource/api1600/synergy/volume_attachment_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-RSpec.describe OneviewSDK::API1200::Synergy::VolumeAttachment do
+RSpec.describe OneviewSDK::API1600::Synergy::VolumeAttachment do
   include_context 'shared context'
 
-  it 'inherits from OneviewSDK::API1200::C7000::VolumeAttachment' do
-    expect(described_class).to be < OneviewSDK::API1200::C7000::VolumeAttachment
+  it 'inherits from OneviewSDK::API1600::C7000::VolumeAttachment' do
+    expect(described_class).to be < OneviewSDK::API1600::C7000::VolumeAttachment
   end
 end
