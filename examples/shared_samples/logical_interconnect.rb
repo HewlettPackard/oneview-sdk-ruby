095ad7ff0d25d68be4b08960c8e1ee44c39a7b51
Chebrolu <bala-sai-harika.chebrolu@hpe.com>
2020-05-26 11:00:51 +0530

    Changed comments section in example file

--- examples/shared_samples/logical_interconnect.rb
+++ examples/shared_samples/logical_interconnect.rb
@@ -1,4 +1,4 @@
-# (C) Copyright 2017 Hewlett Packard Enterprise Development LP
+# (C) Copyright 2020 Hewlett Packard Enterprise Development LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # You may not use this file except in compliance with the License.
@@ -14,16 +14,12 @@ require_relative '../_client' # Gives access to @client
 # Example: Explores functionalities of Logical Interconnects
 #
 # Supported APIs:
-# - 200, 300, 500, 600
+# - 200, 300, 500, 600, 800, 1000, 1200
+# Supported Variants:
+# - C7000, Synergy
 
 # Resources that can be created according to parameters:
-# api_version = 200 & variant = any to OneviewSDK::API200::LogicalInterconnect
-# api_version = 300 & variant = C7000 to OneviewSDK::API300::C7000::LogicalInterconnect
-# api_version = 300 & variant = Synergy to OneviewSDK::API300::Synergy::LogicalInterconnect
-# api_version = 500 & variant = C7000 to OneviewSDK::API500::C7000::LogicalInterconnect
-# api_version = 500 & variant = Synergy to OneviewSDK::API500::Synergy::LogicalInterconnect
-# api_version = 600 & variant = C7000 to OneviewSDK::API600::C7000::LogicalInterconnect
-# api_version = 600 & variant = Synergy to OneviewSDK::API600::Synergy::LogicalInterconnect
+# for example, if api_version = 800 & variant = C7000 to OneviewSDK::API800::C7000::LogicalInterconnect
 
 # Resource Class used in this sample
 logical_interconnect_class = OneviewSDK.resource_named('LogicalInterconnect', @client.api_version)
