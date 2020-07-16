69f887a7428e8c76aebe2dd7d854b6d76ac7eb1b
yuvirani <yuvaranisrec@gmail.com>
2020-07-16 12:22:09 +0530

    Fix versions in spec files

--- spec/unit/resource/api1800/synergy/enclosure_spec.rb
+++ spec/unit/resource/api1800/synergy/enclosure_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-RSpec.describe OneviewSDK::API1600::Synergy::Enclosure do
+RSpec.describe OneviewSDK::API1800::Synergy::Enclosure do
   include_context 'shared context'
 
-  it 'inherits from OneviewSDK::API1200::Synergy::Enclosure' do
-    expect(described_class).to be < OneviewSDK::API1200::Synergy::Enclosure
+  it 'inherits from OneviewSDK::API1600::Synergy::Enclosure' do
+    expect(described_class).to be < OneviewSDK::API1600::Synergy::Enclosure
   end
 end
