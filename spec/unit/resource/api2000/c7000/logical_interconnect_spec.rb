f238c2a426554caedbacd89b7068be3fb52ecf37
yuvirani <yuvaranisrec@gmail.com>
2020-09-11 09:37:32 +0530

    test fix

--- spec/unit/resource/api2000/c7000/logical_interconnect_spec.rb
+++ spec/unit/resource/api2000/c7000/logical_interconnect_spec.rb
@@ -30,9 +30,9 @@ RSpec.describe OneviewSDK::API2000::C7000::LogicalInterconnect do
     it 'gets the inconsistency report for bulk update' do
       item = log_int
       options = {
-        "logicalInterconnectUris" => []
-      } 
-      expect(@client_2000).to receive(:rest_post).with(item.class::BASE_URI + '/bulk-inconsistency-validate', { "body" => options }, item.api_version)
+        'logicalInterconnectUris' => []
+      }
+      expect(@client_2000).to receive(:rest_post).with(item.class::BASE_URI + '/bulk-inconsistency-validate', { 'body' => options }, item.api_version)
                                                  .and_return(FakeResponse.new)
       item.bulk_inconsistency_validate
     end
