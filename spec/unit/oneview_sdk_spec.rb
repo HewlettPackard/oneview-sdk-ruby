f397af2372261aa3ca2a28b5ea2296172af35932
VenkateshRavula <39786905+VenkateshRavula@users.noreply.github.com>
2020-11-03 21:14:23 +0530

    Update oneview_sdk_spec.rb
    change api version to 2200 in oneview_sd_spec.rb

--- spec/unit/oneview_sdk_spec.rb
+++ spec/unit/oneview_sdk_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe OneviewSDK do
 
   it 'raises an error when an invalid API300 version is called' do
     expect { OneviewSDK::API999 }.to raise_error(NameError,
-                                                 'The API999 method or resource does not exist for OneView API version 2000.')
+                                                 'The API999 method or resource does not exist for OneView API version 2200.')
   end
 
   it 'has a default api version' do
