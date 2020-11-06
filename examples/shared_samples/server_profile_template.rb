f45d3c0e6b02a76e5892a64425e8c44f4bfabfc8
VenkateshRavula <venkatesh.ravula@hpe.com>
2020-11-06 15:41:49 +0530

    modify spt example to support hcp

--- examples/shared_samples/server_profile_template.rb
+++ examples/shared_samples/server_profile_template.rb
@@ -20,10 +20,31 @@ server_profile_template_class = OneviewSDK.resource_named('ServerProfileTemplate
 # Extra classes used in this sample
 enclosure_group_class = OneviewSDK.resource_named('EnclosureGroup', @client.api_version)
 server_hardware_type_class = OneviewSDK.resource_named('ServerHardwareType', @client.api_version)
+ethernet_network_class = OneviewSDK.resource_named('EthernetNetwork', @client.api_version)
 
-server_profile_template_name = 'OneViewSDK Test ServerProfileTemplate'
+mgmt_name = 'mgmt_nw'
+eth_nw = ethernet_network_class.new(@client, name: mgmt_name)
+eth_nw.retrieve!
+
+server_profile_template_name = @hypervisor_serverProfileTemplate
 puts "\n### Creating a new Server Profile Template based on a Server Hardware Type and Enclosure Group"
-item = server_profile_template_class.new(@client, name:  server_profile_template_name)
+
+options = {
+  name: server_profile_template_name,
+  connectionSettings: {
+    connections: [{
+      name: 'mgmt',
+      id: 1,
+      functionType: 'Ethernet',
+      portId: 'Auto',
+      networkUri: eth_nw[:uri]
+    }],
+    manageConnections: true,
+    complianceControl: 'Checked'
+  }
+}
+
+item = server_profile_template_class.new(@client, options)
 
 server_hardware_type = server_hardware_type_class.find_by(@client, {}).first
 raise 'Failed to find Server Hardware Type' unless server_hardware_type || server_hardware_type['uri']
@@ -32,6 +53,7 @@ enclosure_group = enclosure_group_class.find_by(@client, {}).first
 raise 'Failed to find Enclosure Group' unless enclosure_group || enclosure_group['uri']
 item.set_enclosure_group(enclosure_group)
 item.create
+
 puts "\nCreated Server Profile Template '#{item['name']}' successfully.\n  uri = '#{item['uri']}'"
 puts "\nServer Hardware Type '#{server_hardware_type['name']}'.\n uri = '#{item['serverHardwareTypeUri']}'"
 puts "\nEnclosure Group '#{enclosure_group['name']}'.\n  uri = '#{item['enclosureGroupUri']}'"
@@ -81,7 +103,7 @@ rescue NoMethodError
   puts "\nThe method #get_available_networks is available from API 600 onwards."
 end
 
-puts "\n\n### Deleting all Server Profiles Template created in this sample"
-item2.delete
+puts "\n\n### Deleting Server Profiles Template created in this sample"
+# item2.delete
 item3.delete
 puts "\nServer Profiles Template removed successfully."
