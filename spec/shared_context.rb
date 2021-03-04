52b9487b7ff56e4b3699ee1cd01acde8b44363ad
Harika <chebroluharika@gmail.com>
2021-03-04 19:42:07 +0530

    PR for config changes #pr

--- spec/shared_context.rb
+++ spec/shared_context.rb
@@ -138,6 +138,15 @@ RSpec.shared_context 'integration api2400 context', a: :b do
   end
 end
 
+# Context for API2600 integration testing:
+RSpec.shared_context 'integration api2600 context', a: :b do
+  before :all do
+    integration_context
+    $client_2600 ||= OneviewSDK::Client.new($config.merge(api_version: 2600))
+    $client_2600_synergy ||= OneviewSDK::Client.new($config_synergy.merge(api_version: 2600))
+  end
+end
+
 # Context for Image Streamer API300 integration testing:
 RSpec.shared_context 'integration i3s api300 context', a: :b do
   before :all do
@@ -249,6 +258,13 @@ RSpec.shared_context 'system api2400 context', a: :b do
   before(:each) do
     load_system_properties
     generate_clients(2400)
+  end
+end
+
+RSpec.shared_context 'system api2600 context', a: :b do
+  before(:each) do
+    load_system_properties
+    generate_clients(2600)
   end
 end
 
@@ -386,6 +402,9 @@ def generate_clients(api_version)
   when 2400
     $client_2400 ||= OneviewSDK::Client.new($config.merge(api_version: api_version))
     $client_2400_synergy ||= OneviewSDK::Client.new($config_synergy.merge(api_version: api_version))
+  when 2600
+    $client_2600 ||= OneviewSDK::Client.new($config.merge(api_version: api_version))
+    $client_2600_synergy ||= OneviewSDK::Client.new($config_synergy.merge(api_version: api_version))
   end
 
   allow_any_instance_of(OneviewSDK::Client).to receive(:appliance_api_version).and_call_original
